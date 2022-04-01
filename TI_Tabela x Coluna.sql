01ea7de8a1108ca3a26dc98ad9ab4532a0b78a6e
Giuliano Gomes <91477669+Giulianogms@users.noreply.github.com>
2022-04-01 10:51:26 -0300

    Update and rename Tabela x Coluna.sql to TI_Tabela x Coluna.sql

--- TI_Tabela x Coluna.sql
+++ TI_Tabela x Coluna.sql
@@ -3,4 +3,5 @@ ALTER SESSION SET current_schema = CONSINCO;
 SELECT owner, table_name, column_name 
 FROM all_tab_columns 
 WHERE column_name LIKE '%NROEMPRESA%' -- EXEMPLO NRO DA EMPRESA
-AND OWNER IN ('CONSINCO');
+AND OWNER IN ('CONSINCO')
+ORDER BY 2;
