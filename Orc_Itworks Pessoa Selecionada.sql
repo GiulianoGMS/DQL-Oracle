9686e17d3d216b69320e04bf5e51f33f46e02ec6
Giuliano Gomes <91477669+Giulianogms@users.noreply.github.com>
2022-05-11 17:34:52 -0300

    Update Orc_Itworks Pessoa Selecionada.sql

--- Orc_Itworks Pessoa Selecionada.sql
+++ Orc_Itworks Pessoa Selecionada.sql
@@ -1,11 +1,39 @@
-ALTER SESSION SET current_schema = CONSINCO;
+create or replace view consinco.nagv_pessoaitworks as
+(
 
-SELECT T.* 
-FROM CONSINCO.NAGV_PESSOAITWORKS T
-WHERE T.RAZAO_SOCIAL <> ('CADASTRADO PELA LEITURA DO CAIXA')
-    AND LPAD(T.CNPJCPF,14,0) = ((SELECT LPAD(ge.nrocgccpf,12,0)||LPAD(GE.DIGCGCCPF,2,0) 
-    FROM CONSINCO.GE_PESSOA GE WHERE GE.SEQPESSOA = :NR1));
+select   --LPAD(ge.nrocgccpf,12,0)||LPAD(GE.DIGCGCCPF,2,0) CNPJCPF -- -Ticket 17469 Solicitação Sirlene
+         --CASE WHEN GE.FISICAJURIDICA = 'F' THEN ge.nrocgccpf||LPAD(GE.DIGCGCCPF,2,0) ELSE LPAD(ge.nrocgccpf,12,0)||LPAD(GE.DIGCGCCPF,2,0) END CNPJCPF, -- Ticket 28250 Solicitação Sirlene
+           CASE WHEN GE.FISICAJURIDICA = 'F' THEN LPAD(ge.nrocgccpf,9,0)||LPAD(GE.DIGCGCCPF,2,0) ELSE LPAD(ge.nrocgccpf,12,0)||LPAD(GE.DIGCGCCPF,2,0) END CNPJCPF, -- Ticket 49294 Solic. Silene 11/05/22
+           GE.NOMERAZAO RAZAO_SOCIAL,
+           GE.LOGRADOURO ENDERECO,
+           GE.NROLOGRADOURO NUMERO,
+           GE.BAIRRO BAIRRO,
+           case when ge.fisicajuridica = 'F' then 'SEM_IE' else GE.INSCRICAORG end IE_RG, --- Ticket 26437 Alteração 15/03/2022 Cipolla -- Ticket 48143 04/05/2022 Cipolla
+           GE.CMPLTOLOGRADOURO COMPLEMENTO,
+           GE.FONEDDD1|| GE.FONENRO1 DDD_FONE,
+           GE.CIDADE CIDADE,
+             (select F.CODIBGE from CONSINCO.GE_CIDADE F WHERE F.SEQCIDADE = GE.SEQCIDADE) CODIGO_CIDADE,
+           GE.PAIS PAIS,
+               (select F.CODPAIS from CONSINCO.GE_CIDADE F WHERE F.SEQCIDADE = GE.SEQCIDADE) CODIGO_PAIS,
+           GE.CEP CEP,
+           GE.UF UF,
+           LPAD(ge.Nrocpfprodutor,9,0)||LPAD(GE.DIGCPFPRODUTOR,2,0) CPFProdutorRural,
+           NULL Ativ39ICMS,
+           NULL OptanteSimplesNacionalUltrapassaLimite,
+           NULL OptanteSimplesNacionalAliquotaAtual,
+           GE.DTAINCLUSAO DTA_INCLUSAO,
+           NVL(GE.DTAALTERACAO,GE.DTAINCLUSAO ) DTA_ALTERACAO,
 
+ --- Adicionado as horas abaixo para tratar no robo da Itworks, para sincronizar tem que ter hora,
+/*        case when TO_CHAR(GE.DTAINCLUSAO,'hh24:mi:ss') = '00:00:00' then
+                            TO_DATE(GE.DTAINCLUSAO,'DD/MM/RRRR') +  7/24  else
+                            GE.DTAINCLUSAO end DTA_INCLUSAO,
 
-    
-    
+          case when  TO_CHAR(NVL(GE.DTAALTERACAO,GE.DTAINCLUSAO ),'hh24:mi:ss') = '00:00:00' then
+                                 TO_DATE(nvl(GE.DTAALTERACAO,GE.DTAINCLUSAO),'DD/MM/RRRR') +  7/24  else
+                             NVL(GE.DTAALTERACAO,GE.DTAINCLUSAO ) end DTA_ALTERACAO,*/
+
+           'View Nagumo' ORIGEM
+from consinco.ge_pessoa ge
+where ge.nrocgccpf is not null)
+;
