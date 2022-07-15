ebce76eff84144b4a0e4fe2087ec846f375a3b65
Giuliano Gomes <91477669+Giulianogms@users.noreply.github.com>
2022-07-15 15:52:37 -0300

    Update Cad_Relatorio Fornecedor.sql

--- Cad_Relatorio Fornecedor.sql
+++ Cad_Relatorio Fornecedor.sql
@@ -10,22 +10,22 @@ select distinct (d.seqproduto),
                 a.principal,
                 decode(w.statuscompra, 'I', 'INATIVO', 'A', 'ATIVO') STATUS_COMPRA,
                 y.descricao DESC_SEGMENTO,
-                CASE WHEN F.QTDEMBALAGEM = 1 THEN (SELECT SUM(AA.ESTOQUE) FROM MRL_PRODLOCAL AA, MRL_LOCAL BB, MRL_PRODUTOEMPRESA CC
+                NVL(((SELECT SUM(AA.ESTOQUE) FROM MRL_PRODLOCAL AA, MRL_LOCAL BB, MRL_PRODUTOEMPRESA CC
                      WHERE AA.SEQLOCAL = BB.SEQLOCAL
                      AND AA.SEQPRODUTO = D.SEQPRODUTO
-                     AND BB.TIPLOCAL = 'L' AND aA.nroempresa = bB.nroempresa and cC.seqproduto = Aa.seqproduto and cC.nroempresa = Aa.nroempresa) ELSE NULL END ESTOQUE_LOJA,
-                CASE WHEN F.QTDEMBALAGEM = 1 THEN (SELECT SUM(AA.ESTOQUE) FROM MRL_PRODLOCAL AA, MRL_LOCAL BB, MRL_PRODUTOEMPRESA CC
+                     AND BB.TIPLOCAL = 'L' AND aA.nroempresa = bB.nroempresa and cC.seqproduto = Aa.seqproduto and cC.nroempresa = Aa.nroempresa) / F.QTDEMBALAGEM),0) ESTOQUE_LOJA,
+                NVL(((SELECT SUM(AA.ESTOQUE) FROM MRL_PRODLOCAL AA, MRL_LOCAL BB, MRL_PRODUTOEMPRESA CC
                      WHERE AA.SEQLOCAL = BB.SEQLOCAL
                      AND AA.SEQPRODUTO = D.SEQPRODUTO
-                     AND BB.TIPLOCAL = 'T' AND aA.nroempresa = bB.nroempresa and cC.seqproduto = Aa.seqproduto and cC.nroempresa = Aa.nroempresa) ELSE NULL END ESTOQUE_AVARIA,    
-                CASE WHEN F.QTDEMBALAGEM = 1 THEN (SELECT SUM(AA.ESTOQUE) FROM MRL_PRODLOCAL AA, MRL_LOCAL BB, MRL_PRODUTOEMPRESA CC
+                     AND BB.TIPLOCAL = 'T' AND aA.nroempresa = bB.nroempresa and cC.seqproduto = Aa.seqproduto and cC.nroempresa = Aa.nroempresa) / F.QTDEMBALAGEM),0)ESTOQUE_AVARIA,    
+                NVL(((SELECT SUM(AA.ESTOQUE) FROM MRL_PRODLOCAL AA, MRL_LOCAL BB, MRL_PRODUTOEMPRESA CC
                      WHERE AA.SEQLOCAL = BB.SEQLOCAL
                      AND AA.SEQPRODUTO = D.SEQPRODUTO
-                     AND BB.TIPLOCAL = 'O' AND aA.nroempresa = bB.nroempresa and cC.seqproduto = Aa.seqproduto and cC.nroempresa = Aa.nroempresa) ELSE NULL END ESTOQUE_OUTROS,    
-                CASE WHEN F.QTDEMBALAGEM = 1 THEN (SELECT SUM(AA.ESTOQUE) FROM MRL_PRODLOCAL AA, MRL_LOCAL BB, MRL_PRODUTOEMPRESA CC
+                     AND BB.TIPLOCAL = 'O' AND aA.nroempresa = bB.nroempresa and cC.seqproduto = Aa.seqproduto and cC.nroempresa = Aa.nroempresa) / F.QTDEMBALAGEM),0) ESTOQUE_OUTROS,    
+                NVL(((SELECT SUM(AA.ESTOQUE) FROM MRL_PRODLOCAL AA, MRL_LOCAL BB, MRL_PRODUTOEMPRESA CC
                      WHERE AA.SEQLOCAL = BB.SEQLOCAL
                      AND AA.SEQPRODUTO = D.SEQPRODUTO
-                     AND BB.TIPLOCAL = 'D' AND aA.nroempresa = bB.nroempresa and cC.seqproduto = Aa.seqproduto and cC.nroempresa = Aa.nroempresa) ELSE NULL END ESTOQUE_CD
+                     AND BB.TIPLOCAL = 'D' AND aA.nroempresa = bB.nroempresa and cC.seqproduto = Aa.seqproduto and cC.nroempresa = Aa.nroempresa) / F.QTDEMBALAGEM),0)ESTOQUE_CD
                 
   from consinco.map_famfornec      a,
        consinco.map_familia        b,
@@ -51,8 +51,8 @@ select distinct (d.seqproduto),
    and e.tipcodigo in ('E', 'D', 'F')
    and w.seqproduto = d.seqproduto
    and w.seqproduto = e.seqproduto
-   and w.nroempresa = 501
-   -nd NVL(y.descricao,'OUTROS') in (#LS2)  
+   and w.nroempresa = 501   
+   and NVL(y.descricao,'OUTROS') in (#LS2)  
    and a.seqfornecedor = :NR1
    and g.comprador in (#LS1)
  order by d.seqproduto, d.desccompleta, 4 DESC;
