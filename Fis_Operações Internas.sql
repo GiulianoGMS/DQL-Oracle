f056d1f0e0a19b61740777b8fadaae0614bd1bad
Giuliano Gomes <91477669+Giulianogms@users.noreply.github.com>
2022-07-21 12:43:57 -0300

    Update Fis_Operações Internas.sql

--- Fis_Operações Internas.sql
+++ Fis_Operações Internas.sql
@@ -1,6 +1,6 @@
-ALTER SESSION SET current_schema = CONSINCO;
+SELECT LOJA, DATA_INICIO, DATA_FIM, CGO, DESCRICAO, TO_CHAR(VALOR,'FM999G999G990D90', 'NLS_NUMERIC_CHARACTERS='',.''') VALOR FROM(
 
-select x.nroempresa LOJA , TO_CHAR(:DT3,'DD/MM/YYYY') DATA_INICIO, TO_CHAR(:DT4,'DD/MM/YYYY') DATA_FIM, x.codgeraloper CGO , y.descricao,  round(sum (x.vlroperacao),2) VALOR
+Select x.nroempresa LOJA , TO_CHAR(:DT3,'DD/MM/YYYY') DATA_INICIO, TO_CHAR(:DT4,'DD/MM/YYYY') DATA_FIM, x.codgeraloper CGO , y.descricao,  round(sum (x.vlroperacao),2) VALOR
 from fatog_vendadia x INNER join dim_codgeraloper y on (x.codgeraloper = y.codgeraloper)
 where x.codgeraloper in (806,807)
 and x.dtaoperacao between  :DT3 and :DT4
@@ -38,4 +38,18 @@ where x.dtaoperacao   between  :DT1 and :DT2
 AND Y.CODGERALOPER NOT IN (49,269,270,271,272,273,274)
 and x.nroempresa IN (#LS1)
 
+UNION ALL
+
+SELECT NULL, NULL, NULL, NULL, 'Diferença', ((select round(sum (x.vlroperacao),2) VALOR
+from fatog_vendadia x INNER join dim_codgeraloper y on (x.codgeraloper = y.codgeraloper)
+where x.codgeraloper in (806,807)
+and x.dtaoperacao between  :DT3 and :DT4
+and x.nroempresa IN (#LS1)) -
+
+(select round(sum(x.valorlanctobrt *x.qtdlancto),2) VALOR
+from fato_perda x INNER join dim_codgeraloper y on (x.codgeraloper = y.codgeraloper)
+where x.dtaoperacao   between  :DT1 and :DT2
+AND Y.CODGERALOPER NOT IN (49,269,270,271,272,273,274)
+and x.nroempresa IN (#LS1))) VALOR FROM DUAL
+)
 order by 1,4
