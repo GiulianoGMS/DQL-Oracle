8118118c7a86349ae19944f4ca64c791bd89e26f
Giuliano Gomes <91477669+Giulianogms@users.noreply.github.com>
2022-07-22 13:54:15 -0300

    Update Fis_Operações Internas.sql

--- Fis_Operações Internas.sql
+++ Fis_Operações Internas.sql
@@ -1,55 +1,67 @@
-SELECT LOJA, DATA_INICIO, DATA_FIM, CGO, DESCRICAO, TO_CHAR(VALOR,'FM999G999G990D90', 'NLS_NUMERIC_CHARACTERS='',.''') VALOR FROM(
+SELECT LOJA, TO_CHAR(DATA_INICIO 'DD-MM-YYYY') DATA_INICIO, TO_CHAR(DATA_FIM, 'DD-MM-YYYY') DATA_FIM,
+       CGO, DESCRICAO, TO_CHAR(VALOR,'FM999G999G990D90', 'NLS_NUMERIC_CHARACTERS='',.''') VALOR FROM(
 
-Select x.nroempresa LOJA , TO_CHAR(:DT3,'DD/MM/YYYY') DATA_INICIO, TO_CHAR(:DT4,'DD/MM/YYYY') DATA_FIM, x.codgeraloper CGO , y.descricao,  round(sum (x.vlroperacao),2) VALOR
+Select x.nroempresa LOJA , TRUNC(X.DTAOPERACAO,'MM') DATA_INICIO, LAST_DAY(TRUNC(X.DTAOPERACAO,'MM')) DATA_FIM,
+x.codgeraloper CGO , y.descricao,  round(sum (x.vlroperacao),2) VALOR
 from fatog_vendadia x INNER join dim_codgeraloper y on (x.codgeraloper = y.codgeraloper)
 where x.codgeraloper in (806,807)
-and x.dtaoperacao between  :DT3 and :DT4
-and x.nroempresa IN (#LS1)
-group by  x.nroempresa, x.codgeraloper, y.descricao
+AND TO_CHAR(X.DTAOPERACAO, 'YYYY') = #LS3
+and TO_CHAR(x.dtaoperacao,'MM') = REGEXP_SUBSTR(#LS2, '(\S*)(\S)') +1
+and x.nroempresa IN (10)
+group by  x.nroempresa, x.codgeraloper, y.descricao, TRUNC(X.DTAOPERACAO,'MM')
 
 union all
 
-select x.nroempresa LOJA , TO_CHAR(:DT1,'DD/MM/YYYY') DATA_INICIO, TO_CHAR(:DT2,'DD/MM/YYYY') DATA_FIM, x.codgeraloper CGO , y.descricao,   round(sum(x.valorlanctobrt *x.qtdlancto),2) VALOR
+select x.nroempresa LOJA , TRUNC(X.DTAOPERACAO,'MM') DATA_INICIO,LAST_DAY(TRUNC(X.DTAOPERACAO,'MM')) DATA_FIM,
+x.codgeraloper CGO , y.descricao,   round(sum(x.valorlanctobrt *x.qtdlancto),2) VALOR
 from fato_perda x INNER join dim_codgeraloper y on (x.codgeraloper = y.codgeraloper)
-where x.dtaoperacao   between  :DT1 and :DT2
-and x.nroempresa IN (#LS1)
+where 
+    TO_CHAR(X.DTAOPERACAO, 'YYYY') = #LS3
+and TO_CHAR(x.dtaoperacao,'MM') = REGEXP_SUBSTR(#LS2, '(\S*)(\S)')
+and x.nroempresa IN (10) 
 AND Y.CODGERALOPER NOT IN (49,269,270,271,272,273,274)
-group by x.nroempresa  , x.codgeraloper  , y.descricao
+group by x.nroempresa  , x.codgeraloper  , y.descricao, TRUNC(X.DTAOPERACAO,'MM')
 
 union all
 
-select null , null , null,  NULL, NULL,null  from dual 
+select null , null , null,null,  NULL,null  from dual
 union all
 
-select null , null , null,  null, NULL,NULL  from dual 
+select null , null , null, null,   NULL,NULL  from dual
 union all
 
-select null , null , NULL,NULL,'Notas Emitidas',  round(sum (x.vlroperacao),2) VALOR
+select null , null ,null, NULL,'Notas Emitidas',  round(sum (x.vlroperacao),2) VALOR
 from fatog_vendadia x INNER join dim_codgeraloper y on (x.codgeraloper = y.codgeraloper)
 where x.codgeraloper in (806,807)
-and x.dtaoperacao between  :DT3 and :DT4
-and x.nroempresa IN (#LS1)
+AND TO_CHAR(X.DTAOPERACAO, 'YYYY') = #LS3
+and TO_CHAR(x.dtaoperacao,'MM') = REGEXP_SUBSTR(#LS2, '(\S*)(\S)') +1
+and x.nroempresa IN (10)
 
 union all
 
-select null , null , NULL,NULL,'Operações Internas',   round(sum(x.valorlanctobrt *x.qtdlancto),2) VALOR
+select null , null , null, NULL,'Operações Internas',   round(sum(x.valorlanctobrt *x.qtdlancto),2) VALOR
 from fato_perda x INNER join dim_codgeraloper y on (x.codgeraloper = y.codgeraloper)
-where x.dtaoperacao   between  :DT1 and :DT2
+where 
+    TO_CHAR(X.DTAOPERACAO, 'YYYY') = #LS3
+and TO_CHAR(x.dtaoperacao,'MM') = REGEXP_SUBSTR(#LS2, '(\S*)(\S)')
 AND Y.CODGERALOPER NOT IN (49,269,270,271,272,273,274)
-and x.nroempresa IN (#LS1)
+and x.nroempresa IN (10)
 
 UNION ALL
 
-SELECT NULL, NULL, NULL, NULL, 'Diferença', ((select round(sum (x.vlroperacao),2) VALOR
+SELECT NULL, NULL, null, NULL, 'Diferença', ((select round(sum (x.vlroperacao),2) VALOR
 from fatog_vendadia x INNER join dim_codgeraloper y on (x.codgeraloper = y.codgeraloper)
 where x.codgeraloper in (806,807)
-and x.dtaoperacao between  :DT3 and :DT4
-and x.nroempresa IN (#LS1)) -
+AND TO_CHAR(X.DTAOPERACAO, 'YYYY') = #LS3
+and TO_CHAR(x.dtaoperacao,'MM') = REGEXP_SUBSTR(#LS2, '(\S*)(\S)')+1
+and x.nroempresa IN (10)) -
 
 (select round(sum(x.valorlanctobrt *x.qtdlancto),2) VALOR
 from fato_perda x INNER join dim_codgeraloper y on (x.codgeraloper = y.codgeraloper)
-where x.dtaoperacao   between  :DT1 and :DT2
+where 
+    TO_CHAR(X.DTAOPERACAO, 'YYYY') = #LS3
+and TO_CHAR(x.dtaoperacao,'MM') = REGEXP_SUBSTR(#LS2, '(\S*)(\S)')
 AND Y.CODGERALOPER NOT IN (49,269,270,271,272,273,274)
-and x.nroempresa IN (#LS1))) VALOR FROM DUAL
+and x.nroempresa IN (10))) VALOR FROM DUAL
 )
-order by 1,4
+order by 1,3;
