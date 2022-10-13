0cf6dd1bb9a485d2674b4618bb8f8fe5d1b2582b
Giuliano Gomes <91477669+Giulianogms@users.noreply.github.com>
2022-10-13 16:23:35 -0300

    Update Cad_Update Telefone.sql

--- Cad_Update Telefone.sql
+++ Cad_Update Telefone.sql
@@ -8,10 +8,14 @@ UPDATE GE_PESSOA A SET FONEDDD1 = NULL,
                        FONENRO3 = NULL,
                        FAXDDD   = NULL,
                        FAXNRO   = NULL
-                 WHERE (LENGTH(NVL(A.FONEDDD1,0)) + LENGTH(NVL(A.FONENRO1,0))) < 11
-                 OR FONENRO1 LIKE '%000000000%'
-                 OR FONENRO1 LIKE '%111111111%';
+                 WHERE FISICAJURIDICA = 'F'
+                   AND (LENGTH(NVL(A.FONEDDD1,0)) + LENGTH(NVL(A.FONENRO1,0))) < 11
+                    OR FONENRO1 LIKE '%000000000%'
+                    OR FONENRO1 LIKE '%111111111%';
                    
                    COMMIT;
                    
-SELECT * FROM GE_PESSOA
+SELECT * FROM GE_PESSOA A
+ WHERE (NVL(LENGTH(A.FONEDDD1),0) + NVL(LENGTH(A.FONENRO1),0)) < 11
+                 OR FONENRO1 LIKE '%000000000%'
+                 OR FONENRO1 LIKE '%111111111%'
