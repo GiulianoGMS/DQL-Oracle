16c5bc1dbd3477be7418d9833a960ebbe2d72f2f
Giuliano Gomes <91477669+Giulianogms@users.noreply.github.com>
2022-10-21 17:02:34 -0300

    Update Cad_Update Telefone.sql

--- Cad_Update Telefone.sql
+++ Cad_Update Telefone.sql
@@ -1,14 +1,17 @@
 -- Seta NULO Telefones + DDDs < 11 digitos ou Telefones com '000000000' OU '111111111'
 
-UPDATE GE_PESSOA A SET FONEDDD1 = NULL, FONENRO1 = NULL,
+UPDATE CONSINCO.GE_PESSOA A SET FONEDDD1 = NULL, FONENRO1 = NULL,
                        FONEDDD2 = NULL, FONENRO2 = NULL,
                        FONEDDD3 = NULL, FONENRO3 = NULL,
                        FAXDDD   = NULL, FAXNRO   = NULL
                        
-                 WHERE A.SEQPESSOA = 299441
-                   AND A.FISICAJURIDICA = 'F'
-                   AND (LENGTH(NVL(A.FONEDDD1,0)) + LENGTH(NVL(A.FONENRO1,0))) < 11
-                   /* OR FONENRO1 LIKE '%000000000%'
-                    OR FONENRO1 LIKE '%111111111%';*/ ;
-                   
+                 WHERE -- A.SEQPESSOA = 299441 Exemplo meu próprio cadastro 
+                   AND A.FISICAJURIDICA = 'F' AND (LENGTH(NVL(A.FONEDDD1,0)) + LENGTH(NVL(A.FONENRO1,0))) < 10
+                --  OR A.FISICAJURIDICA = 'F'
+                    OR A.FISICAJURIDICA = 'F' AND FONENRO1 LIKE '%000000000%'  
+                    OR A.FISICAJURIDICA = 'F' AND FONENRO1 LIKE '%111111111%'  ;
+                    
 COMMIT;
+                    
+SELECT * FROM CONSINCO.GE_PESSOA WHERE LENGTH(NVL(A.FONEDDD1,0))
+                   
