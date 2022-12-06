92991b803b75b510a416efd787e92a9966003d1d
Giuliano Gomes <91477669+Giulianogms@users.noreply.github.com>
2022-12-06 16:00:05 -0300

    Update Fis_Update Pauta ST.sql

--- Fis_Update Pauta ST.sql
+++ Fis_Update Pauta ST.sql
@@ -1,13 +1,10 @@
-select MAP_PAUTAUFLOG.INDGERALOGREPLICACAO, MAP_PAUTAUFLOG.NROREGREPLICACAO, MAP_PAUTAUFLOG.SEQPAUTAUFLOG, MAP_PAUTAUFLOG.SEQPAUTA, MAP_PAUTAUFLOG.NROREGTRIBUTACAO, MAP_PAUTAUFLOG.UFORIGEM, MAP_PAUTAUFLOG.UFDESTINO, MAP_PAUTAUFLOG.DTAINICIO, MAP_PAUTAUFLOG.DTAFIM, MAP_PAUTAUFLOG.VLRBASEST, MAP_PAUTAUFLOG.FATORCONVERSAO, MAP_PAUTAUFLOG.APLICPERCACRESCST, MAP_PAUTAUFLOG.TIPCALCPAUTA, MAP_PAUTAUFLOG.SEQPORTARIA, MAP_PAUTAUFLOG.DTAALTERACAO, MAP_PAUTAUFLOG.USUALTERACAO                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
-from MAP_PAUTAUFLOG Where  SEQPAUTA = 5333                           
-       and  nroregtributacao = 2                                  
-       Order By DTAINICIO DESC, DTAFIM DESC
+CREATE TABLE CONSINCO.MAP_PAUTAUFLOG_BACKUP AS 
+
+SELECT * FROM CONSINCO.MAP_PAUTAUFLOG;
        
-UPDATE MAP_PAUTAUFLOG A SET A.TIPCALCPAUTA = 'M',
+UPDATE CONSINCO.MAP_PAUTAUFLOG A SET A.TIPCALCPAUTA = 'M',
                             A.DTAALTERACAO = SYSDATE,
-                            A.USUALTERACAO = 'CONSINCO'
- WHERE A.SEQPAUTA = 5333
-   AND A.NROREGTRIBUTACAO = 2
-   DTAFIM > DATE '2022-09-01';
+                            A.USUALTERACAO = 'TICKET_72781'
+ WHERE DTAFIM >= SYSDATE ;
    
-   COMMIT;
+COMMIT;
