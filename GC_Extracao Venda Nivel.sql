f01a281774014021681e8f427bb11d8da15fda70
Giuliano Gomes <91477669+Giulianogms@users.noreply.github.com>
2023-01-03 19:09:14 -0300

    Update GC_Extracao Venda Nivel.sql

--- GC_Extracao Venda Nivel.sql
+++ GC_Extracao Venda Nivel.sql
@@ -1,24 +1,26 @@
-SELECT TO_CHAR(FV.DTAOPERACAO, 'MM/YYYY') DATA, QC.categoria_nivel_1,
+SELECT TO_CHAR(FV.DTAENTRADASAIDA, 'YYYY') DATA,QC.categoria_nivel_1,
                                                 QC.categoria_nivel_2,
                                                 QC.categoria_nivel_3,
                                                 QC.categoria_nivel_4,
                                                 QC.categoria_nivel_5,
                                                 QC.categoria_nivel_6,
                                                 FV.NROEMPRESA EMPRESA,
-                                                SUM(FV.QTDOPERACAO)       VENDA_QTD,
-                                                SUM(FV.VVLROPERACAOBRUTO) VALOR_TOTAL
-
-FROM FATOG_VENDADIA FV LEFT JOIN QLV_PRODUTO   QP ON QP.seqproduto = FV.SEQPRODUTO
-                       LEFT JOIN QLV_CATEGORIA QC ON QC.seqfamilia = QP.seqproduto
-
-WHERE FV.DTAOPERACAO BETWEEN DATE '2021-01-01' AND DATE '2022-11-30'
-                       
-                       GROUP BY TO_CHAR(FV.DTAOPERACAO, 'MM/YYYY'), QC.categoria_nivel_1,
+                                                SUM(FV.QTDITEM)                                                               VENDA_QTD,
+                                                TO_CHAR(SUM(FV.VLRITEM), 'FM999G999G999D90', 'NLS_NUMERIC_CHARACTERS='',.''') VALOR_TOTAL
+                
+FROM CONSINCO.MADMV_ABCDISTRIBPROD FV --LEFT JOIN QLV_PRODUTO   QP ON QP.seqproduto = FV.SEQPRODUTO
+                       LEFT JOIN QLV_CATEGORIA@CONSINCODW QC ON QC.seqfamilia = FV.SEQFAMILIA
+ 
+WHERE FV.DTAENTRADASAIDA BETWEEN DATE '2022-01-01' AND DATE '2022-11-30'
+  AND FV.NROEMPRESA <= 53 AND FV.NROSEGMENTO IN (1,4,7,2,3)
+  AND FV.CODGERALOPER IN (37,48,123,610,615,613,810,916,910,911)
+  
+                       GROUP BY TO_CHAR(FV.DTAENTRADASAIDA, 'YYYY'),
+                                                QC.categoria_nivel_1,
                                                 QC.categoria_nivel_2,
                                                 QC.categoria_nivel_3,
                                                 QC.categoria_nivel_4,
                                                 QC.categoria_nivel_5,
                                                 QC.categoria_nivel_6,
-                                                QC.categoria_nivel_7,
-                                                FV.NROEMPRESA 
-ORDER BY 1,2,3,4,5,6,7,8
+                                                FV.NROEMPRESA
+                                              
