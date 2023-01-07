50de4591f1d6fac8f35ff6093f67f7e2c4f3a571
Giuliano Gomes <91477669+Giulianogms@users.noreply.github.com>
2023-01-07 00:22:09 -0300

    Update Cad_ANVISA Alfanum.sql

--- Cad_ANVISA Alfanum.sql
+++ Cad_ANVISA Alfanum.sql
@@ -1,6 +1,8 @@
 -- Correção de informações no campo MOTIVOISENCAOMINSAUDE que estiverem com o símbolo º (Simbolo gera erro na integração)
 
 UPDATE CONSINCO.MAP_PRODUTO A SET A.MOTIVOISENCAOMINSAUDE = 'Produtos da linha Pet.Base legal ART.86 LEI N 6360-73'
-WHERE A.MOTIVOISENCAOMINSAUDE LIKE '%Produtos da linha Pet.Base legal ART.86 LEI Nº 6360-73%'
+WHERE A.MOTIVOISENCAOMINSAUDE LIKE '%Produtos da linha Pet.Base legal ART.86 LEI Nº 6360-73%';
+
+COMMIT;
 
 SELECT * FROM CONSINCO.MAP_PRODUTO A WHERE A.MOTIVOISENCAOMINSAUDE LIKE '%º%'
