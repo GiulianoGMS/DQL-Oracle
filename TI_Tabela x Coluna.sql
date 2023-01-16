9c5eb74c894b37e87bddf7f771333c02baedd3d4
Giuliano Gomes <91477669+Giulianogms@users.noreply.github.com>
2023-01-16 17:04:43 -0300

    Delete TI_Tabela x Coluna.sql

--- TI_Tabela x Coluna.sql
+++
@@ -1,7 +0,0 @@
-ALTER SESSION SET current_schema = CONSINCO;
-
-SELECT owner, table_name, column_name 
-FROM all_tab_columns 
-WHERE column_name LIKE '%NROEMPRESA%' -- EXEMPLO NRO DA EMPRESA
-AND OWNER IN ('CONSINCO')
-ORDER BY 2;
