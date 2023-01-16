404f21211776d4fb3966e190f581f4b9a1247008
Giuliano Gomes <91477669+Giulianogms@users.noreply.github.com>
2023-01-16 17:00:31 -0300

    Delete TI_Parametros Dinamicos.sql

--- TI_Parametros Dinamicos.sql
+++
@@ -1,7 +0,0 @@
-SELECT MAX_PARAMETRO.INDREPLICACAO, MAX_PARAMETRO.INDGEROUREPLICACAO, MAX_PARAMETRO.NROEMPRESA, MAX_PARAMETRO.GRUPO, MAX_PARAMETRO.PARAMETRO, MAX_PARAMETRO.TIPODADO, MAX_PARAMETRO.VALOR, MAX_PARAMETRO.DTAHORALTERACAO, MAX_PARAMETRO.USUALTERACAO, MAX_PARAMETRO.COMENTARIO                                                                                                                                                                                                                                                                                                                                                       
-FROM CONSINCO.MAX_PARAMETRO 
-WHERE  (NROEMPRESA IN (SELECT NROEMPRESA FROM CONSINCO.MAX_EMPRESA WHERE STATUS = 'A' ) 
-    OR  NROEMPRESA = 0 )  
-AND COMENTARIO LIKE '%RECEBIMENTO%' -- Desc comentario ou PARAMETRO -- Descricao Parametro
-AND GRUPO =        'GER_COMPRAS'       -- Grupo                  
-ORDER BY PARAMETRO, NROEMPRESA, GRUPO
