cd5932f93ba3ffa638e826fc8f424ac4f35924f7
Giuliano Gomes <91477669+Giulianogms@users.noreply.github.com>
2023-01-16 16:51:04 -0300

    Delete TI_Constraint.sql

--- TI_Constraint.sql
+++
@@ -1,6 +0,0 @@
-SELECT * 
-    FROM all_cons_columns 
-    WHERE constraint_name = ( 
-        SELECT constraint_name FROM all_constraints  
-            WHERE UPPER(table_name) = UPPER('MAP_FAMDIVCATEG') 
-        AND CONSTRAINT_TYPE = 'P');
