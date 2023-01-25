3dbc7853e48349ac8004646b8db174dc54045426
Giuliano Gomes <91477669+Giulianogms@users.noreply.github.com>
2023-01-25 09:28:11 -0300

    Update Fis_Notas Não Lançadas do Grupo.sql

--- Fis_Notas Não Lançadas do Grupo.sql
+++ Fis_Notas Não Lançadas do Grupo.sql
@@ -30,13 +30,15 @@ SELECT D.NROEMPRESA DESTINO,
        CASE WHEN D.NUMERONF IN (SELECT Z.NRONOTA FROM CONSINCO.OR_NFDESPESA Z WHERE Z.NROEMPRESA = D.NROEMPRESA AND Z.NRONOTA = D.NUMERONF AND Z.SEQPESSOA = D.SEQPESSOA)
          THEN 'NF Lançada no Orçamento' ELSE
        DECODE(D.INDPROCESSAMENTO, 'G', 'NF em Conferência', 'L', 'NF Liberada, Conferência Não Iniciada', 'NF Gerada, Processo Não Iniciado') END,
-       D.SEQPESSOA EMITENTE, D.NUMERONF, TO_CHAR(D.DTAEMISSAO, 'DD/MM/YYYY'), NULL
+       D.SEQPESSOA EMITENTE, D.NUMERONF, TO_CHAR(D.DTAEMISSAO, 'DD/MM/YYYY'), 
+       CASE WHEN X.NUMERONF IS NULL THEN NULL ELSE X.NUMERONF||' - CGO: '||X.CODGERALOPER||' - Data Emissão: '
+                                                             ||TO_CHAR(X.DTAEMISSAO, 'DD/MM/YYYY') END NF_MATOU_OP
   FROM MLF_AUXNOTAFISCAL D LEFT JOIN MAX_CODGERALOPER C ON D.CODGERALOPER = C.CODGERALOPER
                            LEFT JOIN MLF_NOTAFISCAL X   ON D.NUMERONF     = SUBSTR(REGEXP_SUBSTR(X.OBSERVACAO, ':[^:](\S*)'),3,10)
                                                     AND X.CODGERALOPER IN (202,16,69,135,22,141,12,169,135,27,23,26)
                                                     AND X.SEQPESSOA    < 999
                                                     AND X.DTAEMISSAO   BETWEEN :DT1 AND :DT2
-                                                    AND D.NROEMPRESA   = X.SEQPESSOA 
+                                                    AND X.NROEMPRESA   = D.SEQPESSOA 
                                                     AND X.STATUSNF != 'C'
  WHERE D.SEQPESSOA < 999 
    AND D.NUMERONF != 0   
