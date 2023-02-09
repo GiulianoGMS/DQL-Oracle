c4688dcc2ec6dd57201c96d254d63a49ced5df20
Giuliano Gomes <91477669+Giulianogms@users.noreply.github.com>
2023-02-08 21:06:58 -0300

    Update Fis_Cartoes V2 QRP.sql

--- Fis_Cartoes V2 QRP.sql
+++ Fis_Cartoes V2 QRP.sql
@@ -1,10 +1,11 @@
-
+ALTER SESSION SET CURRENT_SCHEMA = CONSINCO;
 SELECT EMPRESA, PARTICIPANTE, NOME_PART_PAGTO, PARTICIP_TRANS, NOME_PART_TRANSACAO,
        TO_CHAR(SUM(TOTAL_BRUTO_VENDAS), 'FM999G999G990D90', 'NLS_NUMERIC_CHARACTERS='',.''')        TOTAL_BRUTO_VENDAS,
        TO_CHAR(SUM(TOTAL_PRESTACOES_SERVICOS), 'FM999G999G990D90', 'NLS_NUMERIC_CHARACTERS='',.''') TOTAL_PRESTACOES_SERVICOS,
        TO_CHAR(:DT1, 'DD/MM/YYYY') Data_Inicial, TO_CHAR(:DT2, 'DD/MM/YYYY') Data_Final,
-       SUBTOTAL_VENDAS,
-       SUBTOTAL_SERVICOS, TOTAL
+       TO_CHAR(SUBTOTAL_VENDAS, 'FM999G999G990D90', 'NLS_NUMERIC_CHARACTERS='',.''') SUBTOTAL_VENDAS,
+       TO_CHAR(SUBTOTAL_SERVICOS, 'FM999G999G990D90', 'NLS_NUMERIC_CHARACTERS='',.''') SUBTOTAL_SERVICOS, 
+       TO_CHAR(SUBTOTAL_VENDAS + SUBTOTAL_SERVICOS, 'FM999G999G990D90', 'NLS_NUMERIC_CHARACTERS='',.''') TOTAL
   FROM (
 SELECT EMPRESA, CASE WHEN PARTICIPANTE IN (SELECT LPAD(NROCGCCPF,12,0)||LPAD(DIGCGCCPF,2,0) FROM GE_PESSOA WHERE SEQPESSOA < 999) THEN '60746948150270' ELSE PARTICIPANTE END PARTICIPANTE, 
        CASE WHEN PARTICIPANTE IN (SELECT LPAD(NROCGCCPF,12,0)||LPAD(DIGCGCCPF,2,0) FROM GE_PESSOA WHERE SEQPESSOA < 999) THEN 'BRADESCO S.A' ELSE NOME_PART_PAGTO END NOME_PART_PAGTO, 
@@ -12,8 +13,8 @@ SELECT EMPRESA, CASE WHEN PARTICIPANTE IN (SELECT LPAD(NROCGCCPF,12,0)||LPAD(DIG
        TOTAL_BRUTO_VENDAS, 
        TOTAL_PRESTACOES_SERVICOS, 
        
-      (SELECT TO_CHAR(SUM(TOTAL_BRUTO_VENDAS), 'FM999G999G990D90', 'NLS_NUMERIC_CHARACTERS='',.''')
-             FROM (
+      (SELECT SUM(TOTAL_BRUTO_VENDAS)
+             FROM ( 
              
         SELECT CASE WHEN A.NROEMPRESA >= 500 THEN 0 ELSE SUM(A.VLRORIGINAL) END TOTAL_BRUTO_VENDAS,
                CASE WHEN A.CODESPECIE IN ('SERVRC') THEN SUM(NVL(A.VLRORIGINAL,0)) ELSE 0 END TOTAL_PREST_SERV
@@ -25,10 +26,10 @@ SELECT EMPRESA, CASE WHEN PARTICIPANTE IN (SELECT LPAD(NROCGCCPF,12,0)||LPAD(DIG
              CASE WHEN A.CODESPECIE IN ('IFOOD','IFOPDV') THEN LPAD(B.NROCGCCPF,12,0)||lpad(B.DIGCGCCPF,2,0)
                   WHEN A.CODESPECIE IN ('CCECOM','CDECOM','TKECOM') THEN '14071169000147'
                   ELSE NULL END)
-        WHERE a.codespecie in ('CARDEB', 'CARTAO', 'TICKET', 'CCECOM', 'CDECOM', 'TKECOM','IFOOD','IFOPDV','SERVRC')
-          and a.situacao != 'C'     and B.SEQPESSOA NOT IN (1458426)
+        WHERE a.codespecie in ('CARDEB', 'CARTAO', 'TICKET', 'CCECOM', 'CDECOM', 'TKECOM','IFOOD','IFOPDV','SERVRC','AMERIC')
+          and a.situacao != 'C'     and B.SEQPESSOA NOT IN (1457426,942508)
           and a.dtaemissao BETWEEN :DT1 AND :DT2
-          AND A.NROEMPRESA = :NR1 
+          AND A.NROEMPRESA = :NR1
           GROUP BY A.NROEMPRESA, CODESPECIE
           
  UNION ALL 
@@ -40,7 +41,7 @@ SELECT EMPRESA, CASE WHEN PARTICIPANTE IN (SELECT LPAD(NROCGCCPF,12,0)||LPAD(DIG
                          inner join ge_pessoa b on (x.seqpessoa = b.seqpessoa)
         where x.obrigdireito = 'D'
         and exists (select 1 from fi_compltitulo y where x.seqtitulo = y.seqtitulo and  y.codbarra is not null)
-        and x.seqpessoa <> c.seqpessoa
+        and x.seqpessoa <> c.seqpessoa AND X.SEQPESSOA NOT IN (942508)
         and x.codespecie not in ('SERVRC') 
         and x.apporigem = 'F' 
         AND X.SITUACAO != 'C'
@@ -63,7 +64,7 @@ SELECT EMPRESA, CASE WHEN PARTICIPANTE IN (SELECT LPAD(NROCGCCPF,12,0)||LPAD(DIG
         AND T.NROEMPRESA = :NR1
         AND T.DTALANCTO BETWEEN :DT1 AND :DT2)) SUBTOTAL_VENDAS,
         
-       (SELECT TO_CHAR(SUM(TOTAL_PREST_SERV),   'FM999G999G990D90', 'NLS_NUMERIC_CHARACTERS='',.''') FROM (
+       (SELECT SUM(TOTAL_PREST_SERV) FROM (
 
         SELECT CASE WHEN A.CODESPECIE IN ('SERVRC') THEN SUM(NVL(A.VLRORIGINAL,0)) ELSE 0 END TOTAL_PREST_SERV
 
@@ -74,61 +75,27 @@ SELECT EMPRESA, CASE WHEN PARTICIPANTE IN (SELECT LPAD(NROCGCCPF,12,0)||LPAD(DIG
              CASE WHEN A.CODESPECIE IN ('IFOOD','IFOPDV') THEN LPAD(B.NROCGCCPF,12,0)||lpad(B.DIGCGCCPF,2,0)
                   WHEN A.CODESPECIE IN ('CCECOM','CDECOM','TKECOM') THEN '14071169000147'
                   ELSE NULL END)
-        where a.codespecie in ('CARDEB', 'CARTAO', 'TICKET', 'CCECOM', 'CDECOM', 'TKECOM','IFOOD','IFOPDV','SERVRC')
-          and a.situacao != 'C'     and B.SEQPESSOA NOT IN (1458426)
+        where a.codespecie in ('CARDEB', 'CARTAO', 'TICKET', 'CCECOM', 'CDECOM', 'TKECOM','IFOOD','IFOPDV','SERVRC','AMERIC')
+          and a.situacao != 'C'     and B.SEQPESSOA NOT IN (1457426,942508)
           and a.dtaemissao BETWEEN :DT1 AND :DT2
           AND A.NROEMPRESA = :NR1 
+          
           GROUP BY  CODESPECIE
- )) SUBTOTAL_SERVICOS,
-    (SELECT TO_CHAR(SUM(TOTAL), 'FM999G999G990D90', 'NLS_NUMERIC_CHARACTERS='',.''') TOTAL
+          UNION ALL
+          select sum(nvl(a.vlroriginal,0)) TOT_ISS
 
-FROM (
+      from consinco.fi_titulo a inner join consinco.ge_pessoa b on (a.seqpessoa = b.seqpessoa)
+                                                 inner join consinco.ge_empresa c on (a.nroempresa = c.nroempresa)
 
-        SELECT CASE WHEN A.NROEMPRESA >= 500 THEN 0 ELSE SUM(A.VLRORIGINAL) END + CASE WHEN A.CODESPECIE IN ('SERVRC') THEN SUM(NVL(A.VLRORIGINAL,0)) ELSE 0 END TOTAL
+   where 1=1
+   and a.situacao != 'C'
+   AND A.DTAEMISSAO BETWEEN :DT1 AND :DT2
+   AND A.SEQTITULO IN (
+ SELECT K.SEQTITULO FROM CONSINCO.FI_TITOPERACAO K where K.CODOPERACAO IN (18) AND K.NROPROCESSO IN ( SELECT X.NROPROCESSO
+FROM CONSINCO.FI_TITOPERACAO X WHERE X.CODESPECIEANT = 'SERVRC' AND X.OPCANCELADA IS NULL AND X.CODOPERACAO = 20 AND X.NROEMPRCTAPARTIDA NOT IN (501)))
 
-        FROM CONSINCO.FI_TITULO A
-          INNER JOIN consinco.ge_pessoa b  on (a.seqpessoa = b.seqpessoa)
-          INNER JOIN consinco.ge_empresa c on (a.nroempresa = c.nroempresa)
-          LEFT  JOIN CONSINCO.GE_PESSOA D ON LPAD(D.NROCGCCPF,12,0)||LPAD(D.DIGCGCCPF,2,0) = (
-               CASE WHEN A.CODESPECIE IN ('IFOOD','IFOPDV') THEN LPAD(B.NROCGCCPF,12,0)||lpad(B.DIGCGCCPF,2,0)
-                    WHEN A.CODESPECIE IN ('CCECOM','CDECOM','TKECOM') THEN '14071169000147'
-                    ELSE NULL END)
-          where a.codespecie in ('CARDEB', 'CARTAO', 'TICKET', 'CCECOM', 'CDECOM', 'TKECOM','IFOOD','IFOPDV','SERVRC')
-            and a.situacao != 'C'     and B.SEQPESSOA NOT IN (1458426)
-            and a.dtaemissao BETWEEN :DT1 AND :DT2
-            AND A.NROEMPRESA = :NR1 GROUP BY A.NROEMPRESA, A.CODESPECIE
+ )) SUBTOTAL_SERVICOS
     
- UNION ALL
-    
-          SELECT sum(x.vlroriginal)
-
-          from fi_titulo x inner join fi_especie k on (k.codespecie = x.codespecie and x.nroempresamae = k.nroempresamae )
-                                      inner join ge_empresa c on (c.nroempresa = x.nroempresa)
-                                      inner join ge_pessoa b on (x.seqpessoa = b.seqpessoa)
-          where x.obrigdireito = 'D'
-          and exists (select 1 from fi_compltitulo y where x.seqtitulo = y.seqtitulo and  y.codbarra is not null)
-          and x.seqpessoa <> c.seqpessoa
-          and x.codespecie not in ('SERVRC') 
-          and x.apporigem = 'F' 
-          AND X.SITUACAO != 'C'
-          and nvl(k.espdevolucao,'N') = 'N' 
-          AND X.NROEMPRESA = :NR1
-          AND X.DTAEMISSAO BETWEEN :DT1 AND :DT2
-
- UNION ALL 
-
-          SELECT sum(t.vlrlancamento)
-                 
-          from FI_CTACORLANCA t inner join fi_ctacorrente x on (t.seqctacorrente = x.seqctacorrente)
-                                                      inner join ge_empresa z on (z.nroempresa = t.nroempresa)
-                                                      LEFT JOIN GE_PESSOA C ON x.Seqpessoanota = C.SEQPESSOA
-          where 1=1
-          and x.tipoconta = 'B' 
-          and t.codoperacao in (452, 948) 
-          and t.origemdoc = 'TSN' 
-          and t.opcancelada is null
-          AND T.NROEMPRESA = :NR1
-          AND T.DTALANCTO BETWEEN :DT1 AND :DT2)) TOTAL
 FROM (
 SELECT Y.* FROM (
 SELECT * FROM (
@@ -146,7 +113,8 @@ SELECT A.NROEMPRESA, LPAD(B.NROCGCCPF,12,0)||LPAD(B.DIGCGCCPF,2,0) PARTICIPANTE,
               ELSE NULL END PARTICIP_TRANS,
        SUBSTR(D.NOMERAZAO,1,32) NOME_PART_TRANSACAO,
        CASE WHEN A.NROEMPRESA >= 500 THEN 0 ELSE SUM(A.VLRORIGINAL) END TOTAL_BRUTO_VENDAS,
-       CASE WHEN A.CODESPECIE IN ('SERVRC') THEN SUM(NVL(A.VLRORIGINAL,0)) ELSE 0 END TOTAL_PRESTACOES_SERVICOS
+       CASE WHEN A.CODESPECIE IN ('SERVRC') THEN SUM(NVL(A.VLRORIGINAL,0)) ELSE 0 END 
+       TOTAL_PRESTACOES_SERVICOS
 
   FROM CONSINCO.FI_TITULO A
   INNER JOIN consinco.ge_pessoa b  on (a.seqpessoa = b.seqpessoa)
@@ -156,11 +124,30 @@ SELECT A.NROEMPRESA, LPAD(B.NROCGCCPF,12,0)||LPAD(B.DIGCGCCPF,2,0) PARTICIPANTE,
             WHEN A.CODESPECIE IN ('CCECOM','CDECOM','TKECOM') THEN '14071169000147'
             ELSE NULL END)
 
-  where a.codespecie in ('CARDEB', 'CARTAO', 'TICKET', 'CCECOM', 'CDECOM', 'TKECOM','IFOOD','IFOPDV','SERVRC')
-    and a.situacao != 'C'     and B.SEQPESSOA NOT IN (1458426)
+  where a.codespecie in ('CARDEB', 'CARTAO', 'TICKET', 'CCECOM', 'CDECOM', 'TKECOM','IFOOD','IFOPDV','SERVRC','AMERIC')
+    and a.situacao != 'C'     and B.SEQPESSOA NOT IN (1457426)
     and a.dtaemissao BETWEEN :DT1 AND :DT2
     and A.NROEMPRESA = :NR1
-    GROUP BY LPAD(B.NROCGCCPF,12,0)||LPAD(B.DIGCGCCPF,2,0), B.NOMERAZAO, A.CODESPECIE, D.NOMERAZAO, A.NROEMPRESA
+    and a.seqpessoa not in (942508)
+GROUP BY LPAD(B.NROCGCCPF,12,0)||LPAD(B.DIGCGCCPF,2,0), B.NOMERAZAO, A.CODESPECIE, D.NOMERAZAO, A.NROEMPRESA
+   UNION ALL
+   select A.NROEMPRESA,
+
+         lpad(B.NROCGCCPF,12,0)||lpad(B.DIGCGCCPF,2,0) Cod_PART_IP,
+          B.NOMERAZAO, NULL, NULL,
+          0 ,
+          sum(nvl(a.vlroriginal,0)) TOT_ISS
+
+      from consinco.fi_titulo a inner join consinco.ge_pessoa b on (a.seqpessoa = b.seqpessoa)
+                                                 inner join consinco.ge_empresa c on (a.nroempresa = c.nroempresa)
+
+   where 1=1
+   and a.situacao != 'C'
+   AND A.DTAEMISSAO BETWEEN :DT1 AND :DT2
+   AND A.SEQTITULO IN (
+ SELECT K.SEQTITULO FROM CONSINCO.FI_TITOPERACAO K where K.CODOPERACAO IN (18) AND K.NROPROCESSO IN ( SELECT X.NROPROCESSO
+FROM CONSINCO.FI_TITOPERACAO X WHERE X.CODESPECIEANT = 'SERVRC' AND X.OPCANCELADA IS NULL AND X.CODOPERACAO = 20 AND X.NROEMPRCTAPARTIDA NOT IN (501)))
+   group by  lpad(c.nrocgc,12,0)||lpad(c.digcgc,2,0) ,  to_char(a.dtaemissao, 'YYYY'), to_char(a.dtaemissao, 'MM'), lpad(B.NROCGCCPF,12,0)||lpad(B.DIGCGCCPF,2,0), B.NOMERAZAO, a.codespecie,A.NROEMPRESA,to_char(a.dtaemissao, 'MM-YYYY')
 )
 GROUP BY NROEMPRESA, PARTICIPANTE, NOME_PART_PAGTO, PARTICIP_TRANS, NOME_PART_TRANSACAO
 
@@ -212,6 +199,6 @@ AND T.DTALANCTO BETWEEN :DT1 AND :DT2
 
 GROUP BY EMPRESA, PARTICIPANTE, NOME_PART_PAGTO, PARTICIP_TRANS, NOME_PART_TRANSACAO,
        SUBTOTAL_VENDAS,
-       SUBTOTAL_SERVICOS, TOTAL
+       SUBTOTAL_SERVICOS
 
 ORDER BY 3 ASC
