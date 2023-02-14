bec742db049d64d8ceef4818e72a222af0c68dd8
Giuliano Gomes <91477669+Giulianogms@users.noreply.github.com>
2023-02-14 10:06:01 -0300

    Update Scanntech.sql

--- Scanntech.sql
+++ Scanntech.sql
@@ -1,6 +1,6 @@
 ALTER SESSION SET CURRENT_SCHEMA = CONSINCO;
 
-SELECT DISTINCT G.NOMERAZAO Fornecedor, MP.CODACESSO EAN, M.DESCCOMPLETA DESCRICAO,
+SELECT DISTINCT G.NOMERAZAO Fornecedor, MP.CODACESSO EAN, M.DESCCOMPLETA DESCRICAO, P.SEQPROMOCAO,
        NVL(PI.QTDTOTALVDA/PI.QTDEMBALAGEM,0) QTD_VENDA, PS.PRECOVALIDPROMOC/PI.QTDEMBALAGEM PRECO_PROMO, 
        (PS.PRECOVALIDNORMAL/PI.QTDEMBALAGEM)-(PI.PRECOPROMOCIONAL/PI.QTDEMBALAGEM) Sell_Out, P.DTAINICIO INICIO, P.DTAFIM FIM
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
@@ -13,9 +13,11 @@ FROM MRL_PROMOCAOITEM PI LEFT JOIN MRL_PROMOCAO    P ON PI.NROEMPRESA = P.NROEMP
  
 WHERE P.NROEMPRESA    = '8'                         
   AND NVL( P.TIPOPROMOC, 'N')  IN ('N','F', 'A', 'S') 
-  AND P.SEQPROMOCAO = 144806
   AND P.NROSEGMENTO IN (2,5)
   AND DTAFIM > SYSDATE
+  AND P.SEQPROMOCAO = 145164
+  AND PI.QTDEMBALAGEM = 1
+  AND PS.PRECOVALIDNORMAL > 0
   AND P.DTAGERACAOPROMOC IS NOT NULL -- Promoções Geradas
   
   ORDER BY 1,3 DESC
