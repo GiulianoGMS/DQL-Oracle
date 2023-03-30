995cc22ec2b71cc32c20acd322fdedd6e2e95e6f
Giuliano Gomes <91477669+Giulianogms@users.noreply.github.com>
2023-03-30 12:28:00 -0300

    Create Fis_Incons NCM.sql

---
+++ Fis_Incons NCM.sql
@@ -0,0 +1,21 @@
+ALTER SESSION SET CURRENT_SCHEMA = CONSINCO;
+
+-- Ticket 206722 - Adicionado por Giuliano - Solic Danielle 20/03
+-- Regra: Trava 4 dígitos do NCM diferentes entre XML e C5
+
+SELECT DISTINCT (A.SEQAUXNOTAFISCAL) AS SEQAUXNOTAFISCAL,
+                A.NUMERONF,
+                A.NROEMPRESA,
+                0   AS SEQAUXNFITEM,
+                'L' AS BLOQAUTOR,
+                64  AS CODINCONSISTENC,
+                'Divergencia de NCM - Entrar em contato com Depto. Fiscal' MENSAGEM
+                                 
+  FROM CONSINCO.MLF_AUXNOTAFISCAL A INNER JOIN CONSINCO.MLF_AUXNFITEM B ON A.SEQAUXNOTAFISCAL = B.SEQAUXNOTAFISCAL
+                                    INNER JOIN CONSINCO.MAP_PRODUTO C ON B.SEQPRODUTO = C.SEQPRODUTO
+                                    INNER JOIN CONSINCO.MAP_FAMILIA E ON E.SEQFAMILIA = C.SEQFAMILIA
+                                    INNER JOIN CONSINCO.MAP_FAMDIVISAO D ON D.SEQFAMILIA = E.SEQFAMILIA
+                                    INNER JOIN TMP_M000_NF K ON (K.M000_NR_CHAVE_ACESSO = A.NFECHAVEACESSO)
+                                    INNER JOIN TMP_M014_ITEM L ON (L.M000_ID_NF = K.M000_ID_NF AND L.M014_NR_ITEM = B.SEQITEMNFXML)
+                                    
+WHERE SUBSTR(M014_CD_NCM,0,4) != SUBSTR(E.CODNBMSH,0,4) AND A.SEQPESSOA > 999
