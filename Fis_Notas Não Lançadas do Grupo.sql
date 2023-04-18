910d2c157def9b780e4ca4eb26ded7bded37cbf4
Giuliano Gomes <91477669+Giulianogms@users.noreply.github.com>
2023-04-18 09:10:03 -0300

    Update Fis_Notas Não Lançadas do Grupo.sql

--- Fis_Notas Não Lançadas do Grupo.sql
+++ Fis_Notas Não Lançadas do Grupo.sql
@@ -1,3 +1,5 @@
+ALTER SESSION SET CURRENT_SCHEMA = CONSINCO;
+
 SELECT A.SEQPESSOA EMP_DESTINO, CASE WHEN A.NUMERONF IN(SELECT Z.NRONOTA FROM CONSINCO.OR_NFDESPESA Z WHERE Z.NROEMPRESA = A.SEQPESSOA 
                                    AND Z.NRONOTA = A.NUMERONF AND Z.SEQPESSOA = A.NROEMPRESA)
          THEN 'NF Lançada no Orçamento' ELSE 'NF Não Importada' END STATUS, A.NROEMPRESA EMITENTE, A.NUMERONF, 
@@ -7,19 +9,20 @@ SELECT A.SEQPESSOA EMP_DESTINO, CASE WHEN A.NUMERONF IN(SELECT Z.NRONOTA FROM CO
 
   FROM MLF_NOTAFISCAL A LEFT JOIN MAX_CODGERALOPER C ON A.CODGERALOPER = C.CODGERALOPER
                         LEFT JOIN MLF_NOTAFISCAL X   ON A.NUMERONF     = SUBSTR(REGEXP_SUBSTR(X.OBSERVACAO, ':[^:](\S*)'),3,10)
-                                                    AND X.CODGERALOPER IN (202,16,69,135,22,141,12,169,135,27,23,26)
+                                                    AND X.CODGERALOPER IN (202,16,69,135,22,141,12,169,135,27,23,26,953,923)
                                                     AND X.SEQPESSOA    < 999
-                                                    AND X.DTAEMISSAO   BETWEEN :DT1 AND :DT2
+                                                    AND X.DTAEMISSAO   >= SYSDATE - 3 --:DT1 
                                                     AND A.NROEMPRESA   = X.SEQPESSOA 
                                                     AND X.STATUSNF != 'C'
+                                                    AND REGEXP_LIKE(SUBSTR(REGEXP_SUBSTR(X.OBSERVACAO, ':[^:](\S*)'),3,10), '^[[:digit:]]+$')
  WHERE A.TIPNOTAFISCAL = 'S' 
-   AND A.DTAENTRADA IS NULL
+   AND A.DTAENTRADA IS NULL 
    AND A.SEQPESSOA != A.NROEMPRESA
    AND A.STATUSNF  != 'C'
-   AND A.SEQPESSOA IN (#LS1)
-   AND A.DTAEMISSAO   BETWEEN :DT1 AND :DT2
+   AND A.SEQPESSOA IN (SELECT NROEMPRESA FROM GE_EMPRESA)
+   AND A.DTAEMISSAO  > SYSDATE - 3 --BETWEEN :DT1 AND :DT2 
    AND A.NUMERONF NOT IN (SELECT NUMERONF FROM MLF_NOTAFISCAL B WHERE B.TIPNOTAFISCAL = 'E' 
-                         AND SEQPESSOA < 999 
+                         AND SEQPESSOA < 999   
                          AND DTAENTRADA IS NOT NULL 
                          AND B.SEQPESSOA = A.NROEMPRESA
                          AND STATUSNF != 'C')
@@ -35,15 +38,16 @@ SELECT D.NROEMPRESA DESTINO,
                                                              ||TO_CHAR(X.DTAEMISSAO, 'DD/MM/YYYY') END NF_MATOU_OP
   FROM MLF_AUXNOTAFISCAL D LEFT JOIN MAX_CODGERALOPER C ON D.CODGERALOPER = C.CODGERALOPER
                            LEFT JOIN MLF_NOTAFISCAL X   ON D.NUMERONF     = SUBSTR(REGEXP_SUBSTR(X.OBSERVACAO, ':[^:](\S*)'),3,10)
-                                                    AND X.CODGERALOPER IN (202,16,69,135,22,141,12,169,135,27,23,26)
+                                                    AND X.CODGERALOPER IN (202,16,69,135,22,141,12,169,135,27,23,26,953,923)
                                                     AND X.SEQPESSOA    < 999
-                                                    AND X.DTAEMISSAO   BETWEEN :DT1 AND :DT2
+                                                    AND X.DTAEMISSAO   >= SYSDATE - 3
                                                     AND X.NROEMPRESA   = D.SEQPESSOA 
                                                     AND X.STATUSNF != 'C'
+                                                    AND REGEXP_LIKE(SUBSTR(REGEXP_SUBSTR(X.OBSERVACAO, ':[^:](\S*)'),3,10), '^[[:digit:]]+$')
  WHERE D.SEQPESSOA < 999 
    AND D.NUMERONF != 0   
-   AND D.DTAEMISSAO BETWEEN :DT1 AND :DT2
-   AND D.NROEMPRESA IN (#LS1)
+   AND D.DTAEMISSAO > SYSDATE - 3
+   AND D.NROEMPRESA IN (SELECT NROEMPRESA FROM GE_EMPRESA)
    AND D.STATUSNF != 'C'
    
- ORDER BY 1,2,5 DESC;
+ ORDER BY 1,2,5 DESC; 
