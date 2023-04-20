9bfe16c0e8eb6d8f162fdf330ae1db69d2efc445
Giuliano Gomes <91477669+Giulianogms@users.noreply.github.com>
2023-04-20 14:01:46 -0300

    Create Fis_Incons GNRE Dare.sql

---
+++ Fis_Incons GNRE Dare.sql
@@ -0,0 +1,14 @@
+SELECT DISTINCT (A.SEQAUXNOTAFISCAL) AS SEQAUXNOTAFISCAL,
+                A.NUMERONF,
+                A.NROEMPRESA,
+                0   AS SEQAUXNFITEM,
+                'L' AS BLOQAUTOR,
+                65  AS CODINCONSISTENC,
+                CASE WHEN CODOBRIGREC != '005' AND CODRECEITA NOT IN ('063-2','100-4') THEN 'O Código da obrigação a recolher e o Código da Receita estão incorretos' 
+                  WHEN CODOBRIGREC != '005' THEN 'O Código da obrigação a recolher está incorreto'
+                  WHEN CODRECEITA NOT IN ('063-2','100-4') THEN 'O Código da Receita está incorreto' ELSE NULL END 
+                ||' na Guia de Recolhimento, Verifique!' MENSAGEM
+
+  FROM CONSINCO.MLF_AUXNOTAFISCAL A INNER JOIN CONSINCO.MLF_GNRE B ON A.SEQAUXNOTAFISCAL = B.SEQAUXNOTAFISCAL
+
+WHERE TIPOGUIA = 'R' AND (CODOBRIGREC != '005' OR CODRECEITA NOT IN ('063-2','100-4'))
