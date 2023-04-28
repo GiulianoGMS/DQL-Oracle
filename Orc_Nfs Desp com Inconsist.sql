ec786e52918cae30490a986287e362106d508db2
Giuliano Gomes <91477669+Giulianogms@users.noreply.github.com>
2023-04-28 12:57:29 -0300

    Update Orc_Nfs Desp com Inconsist.sql

--- Orc_Nfs Desp com Inconsist.sql
+++ Orc_Nfs Desp com Inconsist.sql
@@ -1,14 +1,13 @@
-ALTER SESSION SET CURRENT_SCHEMA = CONSINCO;
-
 SELECT X.NRONOTA, X.NROEMPRESA EMPRESA, X.SEQPESSOA||' - '||
-      (SELECT NOMERAZAO FROM GE_PESSOA WHERE SEQPESSOA = X.SEQPESSOA) FORNECEDOR, -- Select para pegar o nome do fornecedor de acordo com o seqpessoa da tabela OR_NFDESPESA
-       Z.MOTIVO INCONSISTENCIA,X.USUINCLUSAO USUARIO_INCLUSAO, 
-       TO_CHAR(X.DTAINCLUSAO, 'DD/MM/YYYY') DATA_INCLUSAO -- TO_CHAR para formatar a data e ficar no formato correto na aplicação
+      (SELECT NOMERAZAO FROM GE_PESSOA WHERE SEQPESSOA = X.SEQPESSOA) FORNECEDOR, 
+       NVL(Z.MOTIVO, 'Inconsistência não identificada') INCONSISTENCIA,X.USUINCLUSAO USUARIO_INCLUSAO, 
+       TO_CHAR(X.DTAINCLUSAO, 'DD/MM/YYYY') DATA_INCLUSAO 
 
-  FROM CONSINCO.OR_NFDESPESA X INNER JOIN CONSINCO.OR_NFDESPESAINCONSIST Z ON X.SEQNOTA = Z.SEQNOTA -- Join para juntar a tabela de notas e a tabela de inconsistências, para pegar o motivo e apenas as notas que possuem inconsistencias
+  FROM CONSINCO.OR_NFDESPESA X LEFT JOIN CONSINCO.OR_NFDESPESAINCONSIST Z ON X.SEQNOTA = Z.SEQNOTA
 
- WHERE Z.DTAASSINOU IS NULL   -- Notas que não tiveram as inconsistencias assinadas
-   AND X.NROEMPRESA IN (#LS1) -- #LS1 é o select que a aplicação faz para buscar as empresas (filtro na query)
-   AND 
+ WHERE Z.DTAASSINOU IS NULL   
+   AND X.SITUACAO != 'I' 
+   AND DTAINCLUSAO BETWEEN :DT1 AND :DT2
+   AND X.NROEMPRESA IN (#LS1) 
  
  ORDER BY 2,1
