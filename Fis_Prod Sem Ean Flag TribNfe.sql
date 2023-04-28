efb2af8c861c95889736f6f594e2b83f2058d031
Giuliano Gomes <91477669+Giulianogms@users.noreply.github.com>
2023-04-28 08:56:22 -0300

    Update Fis_Prod Sem Ean Flag TribNfe.sql

--- Fis_Prod Sem Ean Flag TribNfe.sql
+++ Fis_Prod Sem Ean Flag TribNfe.sql
@@ -1,13 +1,10 @@
 ALTER SESSION SET CURRENT_SCHEMA = CONSINCO;
 
-SELECT X.NRONOTA, X.NROEMPRESA EMPRESA, X.SEQPESSOA||' - '||
-      (SELECT NOMERAZAO FROM GE_PESSOA WHERE SEQPESSOA = X.SEQPESSOA) FORNECEDOR, -- Select para pegar o nome do fornecedor de acordo com o seqpessoa da tabela OR_NFDESPESA
-       Z.MOTIVO INCONSISTENCIA,X.USUINCLUSAO USUARIO_INCLUSAO, 
-       TO_CHAR(X.DTAINCLUSAO, 'DD/MM/YYYY') DATA_INCLUSAO -- TO_CHAR para formatar a data e ficar no formato correto na aplicação
+SELECT DISTINCT X.SEQPRODUTO PLU, DESCCOMPLETA DESCRICAO, INDEANTRIBNFE EAN_TRIB_DANFE 
 
-  FROM CONSINCO.OR_NFDESPESA X INNER JOIN CONSINCO.OR_NFDESPESAINCONSIST Z ON X.SEQNOTA = Z.SEQNOTA -- Join para juntar a tabela de notas e a tabela de inconsistências, para pegar o motivo e apenas as notas que possuem inconsistencias
+  FROM MAP_PRODCODIGO X LEFT JOIN MAP_PRODUTO M ON X.SEQPRODUTO = M.SEQPRODUTO
 
- WHERE Z.DTAASSINOU IS NULL   -- Notas que não tiveram as inconsistencias assinadas
-   AND X.NROEMPRESA IN (#LS1) -- #LS1 é o select que a aplicação faz para buscar as empresas (filtro na query)
- 
- ORDER BY 2,1
+ WHERE X.SEQPRODUTO NOT IN (SELECT DISTINCT SEQPRODUTO FROM MAP_PRODCODIGO Z WHERE Z.TIPCODIGO = 'E' AND Z.INDEANTRIBNFE = 'S' AND Z.SEQPRODUTO = X.SEQPRODUTO)
+   AND X.TIPCODIGO = 'E'
+
+       ORDER BY 2 ASC
