bc15c1528e674af806b4fb3ad2d1ffd4c828d156
Giuliano Gomes <91477669+Giulianogms@users.noreply.github.com>
2023-05-16 10:51:52 -0300

    Update Com_Critica Pzo Pedido.sql

--- Com_Critica Pzo Pedido.sql
+++ Com_Critica Pzo Pedido.sql
@@ -1,5 +1,7 @@
 -- Inserir em MLFV_AUXNOTAFISCALINCONS
 
+UNION ALL -- Trava lançamentos com vencimento menor que a data do pedido - Giuliano - Solicitação Ronie
+
 SELECT DISTINCT(X.SEQAUXNOTAFISCAL) AS SEQAUXNOTAFISCAL,
                 X.NUMERONF,
                 X.NROEMPRESA,
@@ -7,23 +9,26 @@ SELECT DISTINCT(X.SEQAUXNOTAFISCAL) AS SEQAUXNOTAFISCAL,
                 'L' AS BLOQAUTOR,
                 62  AS CODINCONSISTENC,
                 'Prazo de Vencimento Informado MENOR que o Prazo do Pedido' /*- Pedido: '||MP.NROPEDIDOSUPRIM||
-                ' - Prazo: '||MP.PZOPAGAMENTO||TO_CHAR((MP.DTAEMISSAO + MP.PZOPAGAMENTO), 'DD/MM/YYYY')*/ 
+                ' - Prazo: '||MP.PZOPAGAMENTO||TO_CHAR((MP.DTAEMISSAO + MP.PZOPAGAMENTO), 'DD/MM/YYYY')*/
+                -- Irá constar erro caso tenha mais que um pedido divergente
                 AS MENSAGEM
 
   FROM MLF_AUXNOTAFISCAL X INNER JOIN MLF_AUXNFVENCIMENTO MA ON MA.SEQAUXNOTAFISCAL = X.SEQAUXNOTAFISCAL
                            INNER JOIN MLF_AUXNFITEM XA       ON X.SEQAUXNOTAFISCAL  = XA.SEQAUXNOTAFISCAL AND XA.SITUACAONF != 'C'
-                           INNER JOIN MSU_PEDIDOSUPRIM MP    ON XA.NROPEDIDOSUPRIM  = MP.NROPEDIDOSUPRIM  AND MP.SITUACAOPED != 'C' 
+                           INNER JOIN MSU_PEDIDOSUPRIM MP    ON XA.NROPEDIDOSUPRIM  = MP.NROPEDIDOSUPRIM  AND MP.SITUACAOPED != 'C'
                                                                                                           AND MP.CENTRALLOJA = XA.CENTRALLOJA
-                           INNER JOIN MAF_FORNECDIVISAO MF   ON MF.SEQFORNECEDOR    = MP.SEQFORNECEDOR    
-                           
+                                                                                                          AND X.NROEMPRESA = MP.NROEMPRESA
+                           INNER JOIN MAF_FORNECDIVISAO MF   ON MF.SEQFORNECEDOR    = MP.SEQFORNECEDOR
+
  WHERE MA.DTAVENCIMENTO < DECODE(MF.INDPZOPAGAMENTO, 'F', -- Forma de pagamento Faturamento
-                  DECODE(MF.TIPODTABASEVENCTO, 'E', MP.DTAEMISSAO, 'R', MP.DTARECEBTO, MP.DTAEMISSAO) + 
+                  DECODE(MF.TIPODTABASEVENCTO, 'E', X.DTAEMISSAO, 'R', X.DTARECEBIMENTO, 'S', X.DTASAIDA) +
                   CASE WHEN MP.PZOPAGAMENTO LIKE '%/%' THEN REGEXP_SUBSTR(REPLACE(MP.PZOPAGAMENTO, '/',' '), '(\S*)(\s)',1)
                        ELSE MP.PZOPAGAMENTO END,
                   -- Function para tratar outras formas de pagamento de acordo com o cadastro (Fora a Dezena, Quinzena, Semana ou Mês)
-                  FMAD_CALCDTAVENCTO((DECODE(MF.TIPODTABASEVENCTO, 'E', MP.DTAEMISSAO, 'R', MP.DTARECEBTO, MP.DTAEMISSAO)), MF.INDPZOPAGAMENTO,
+                  FMAD_CALCDTAVENCTO((DECODE(MF.TIPODTABASEVENCTO, 'E', X.DTAEMISSAO, 'R', X.DTARECEBIMENTO, 'S', X.DTASAIDA)), MF.INDPZOPAGAMENTO,
                   CASE WHEN MP.PZOPAGAMENTO LIKE '%/%' THEN REGEXP_SUBSTR(REPLACE(MP.PZOPAGAMENTO, '/',' '), '(\S*)(\s)',1)
-                       ELSE MP.PZOPAGAMENTO END, NULL))
-   
-   AND MP.PZOPAGAMENTO IS NOT NULL 
-   AND MP.TIPPEDIDOSUPRIM NOT IN ('X','T') -- AND NUMERONF = 197793 < Teste
+                       ELSE MP.PZOPAGAMENTO END, NULL)) -1 -- 1 Dia de Margem Aceitável
+
+   AND MP.PZOPAGAMENTO IS NOT NULL
+   AND X.NUMERONF != 0
+   AND MP.TIPPEDIDOSUPRIM NOT IN ('X','T')
