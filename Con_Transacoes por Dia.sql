f208abe15b7738f76d311f39c5021ae6915e94d4
Giuliano Gomes <91477669+Giulianogms@users.noreply.github.com>
2023-06-06 18:55:03 -0300

    Create Con_Transacoes por Dia.sql

---
+++ Con_Transacoes por Dia.sql
@@ -0,0 +1,33 @@
+ALTER SESSION SET CURRENT_SCHEMA = CONSINCO;
+
+SELECT A.SEQTITULO, C.NRODOCUMENTO, C.NROEMPRESA, A.NSU, 
+       TO_CHAR(A.DTAMOVIMENTO, 'DD/MM/YYYY') DTAMOVIMENTO,
+       TO_CHAR(A.DTACONCILIADO,'DD/MM/YYYY') DTACONCILIADO, 
+       TO_CHAR(B.DTAPAGAMENTO, 'DD/MM/YYYY') DTAPAGAMENTO, 
+       TO_CHAR((A.VALOR),   'FM999G999G990D90', 'NLS_NUMERIC_CHARACTERS='',.''') VALOR_TOTAL, 
+       TO_CHAR((B.VLRPAGO), 'FM999G999G990D90', 'NLS_NUMERIC_CHARACTERS='',.''') VALOR_SEMTAXA, 
+       TO_CHAR((B.VLRTAXA1),'FM999G999G990D90', 'NLS_NUMERIC_CHARACTERS='',.''') VLRTAXA1, 
+       TO_CHAR((B.VLRTAXA2),'FM999G999G990D90', 'NLS_NUMERIC_CHARACTERS='',.''') VLRTAXA2     
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
+FROM FI_TITULONSU A,FI_TITULO C, 
+ (SELECT X.SEQTITULONSU,
+ X.SEQTITULO,
+ SUM(X.VLRPAGO) VLRPAGO,
+ SUM(X.VLRTAXA1) VLRTAXA1,
+ SUM(X.VLRTAXA2) VLRTAXA2,
+ MIN(X.DTAPAGAMENTO) DTAPAGAMENTO,
+ MIN(X.ARQUIVO) ARQUIVO
+ FROM FIV_LINHAARQUIVOTITNUS X
+ WHERE X.SEQTITULONSU IS NOT NULL
+ AND X.STATUS = 'Q'
+ GROUP BY X.SEQTITULONSU, X.SEQTITULO) B 
+
+WHERE A.SEQTITULO        = B.SEQTITULO(+)
+      AND A.SEQTITULONSU = B.SEQTITULONSU(+)
+      AND A.SEQTITULO    = C.SEQTITULO (+)
+      AND A.DTAMOVIMENTO >= :DT1
+      AND A.DTAMOVIMENTO <= :DT1
+      AND C.NROEMPRESA IN (#LS1)
+      AND C.CODESPECIE IN ( 'AMERIC', 'CARDEB', 'CARTAO', 'CCECOM', 'CDECOM', 'IFOOD', 'IFOPDV', 'TICKET', 'TKECOM',
+                            'ATIVO', 'ATVEFU', 'DESP', 'DUPP', 'FRETE', 'GNRE', 'PAGEST', 'RECARG')
+      ORDER BY 3,1
