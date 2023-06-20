90161b7d7f843e7944bd5a7c9fcb402ed5572b55
Giuliano Gomes <91477669+Giulianogms@users.noreply.github.com>
2023-06-20 12:57:10 -0300

    Update NAGT_ABASTECIMENTOCONFIG.sql

--- NAGT_ABASTECIMENTOCONFIG.sql
+++ NAGT_ABASTECIMENTOCONFIG.sql
@@ -1,9 +1,6 @@
 -- Tabela de Configuração | Abastecimento Automatico
--- Aplicação não vincula Fornecedor ao lote, logo precisa estar configurado nesta tabela
--- e os produtos devem respeitar o fornecedor
 
 CREATE TABLE CONSINCO.NAGT_ABASTECIMENTOCONFIG (
-                  SEQFORNECEDOR NUMBER(20),
                   SEQLOTEMODELO NUMBER(20),
                   DIASEMANA     VARCHAR2(10),
                   DIASCONFIG    NUMBER(3))
