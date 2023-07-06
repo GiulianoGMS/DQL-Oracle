a16b012c1c1461249336118b639d82e33ac32e0c
Giuliano Gomes <91477669+Giulianogms@users.noreply.github.com>
2023-07-06 17:47:53 -0300

    Update NAGF_BUSCAULTDTAPEDIDO.sql

--- NAGF_BUSCAULTDTAPEDIDO.sql
+++ NAGF_BUSCAULTDTAPEDIDO.sql
@@ -1,8 +1,9 @@
-CREATE OR REPLACE FUNCTION NAGF_BUSCAULTDTAPEDIDO (p_SeqLoteModelo NUMBER) RETURN DATE IS
+CREATE OR REPLACE FUNCTION CONSINCO.NAGF_BUSCAULTDTAPEDIDO (p_SeqLoteModelo NUMBER) RETURN DATE IS
                                                                            v_proxped DATE;
                                                                            v_diasconfig NUMBER;
                                                                            v_diasemana VARCHAR2(10);
-                                                                           v_diahoje VARCHAR(10);
+                                                                           v_dia VARCHAR(10);
+                                                                           v_dta DATE;
  BEGIN
   -- Valida Parametrizacoes na NAGT_ABASTECIMENTOCONFIG
   SELECT DIASCONFIG, DECODE(UPPER(DIASEMANA),  'SEGUNDA' , 'MONDAY',
@@ -13,36 +14,39 @@ CREATE OR REPLACE FUNCTION NAGF_BUSCAULTDTAPEDIDO (p_SeqLoteModelo NUMBER) RETUR
                                                'SABADO ' , 'SATURDAY',
                                                'DOMINGO' , 'SUNDAY')
     INTO v_diasconfig, v_diasemana
-    FROM NAGT_ABASTECIMENTOCONFIG X
+    FROM CONSINCO.NAGT_CONTROLELOTECOMPRA X
    WHERE X.SEQLOTEMODELO = p_SeqLoteModelo;
-
-  SELECT TRIM(TO_CHAR(MAX(DTAHORINCLUSAO) + v_diasconfig,'DAY'))
-    INTO v_diahoje
-    FROM CONSINCO.MAC_GERCOMPRA A 
+   
+  SELECT TRIM(TO_CHAR(MAX(TRUNC(DTAHORINCLUSAO)) + v_diasconfig,'DAY')), MAX(TRUNC(DTAHORINCLUSAO))
+    INTO v_dia, v_dta
+    FROM CONSINCO.MAC_GERCOMPRA A
    WHERE 1=1
-     AND A.SEQGERMODELOCOMPRA = p_SeqLoteModelo;
+     AND A.SEQGERMODELOCOMPRA = p_SeqLoteModelo 
+     OR A.SEQGERCOMPRA = p_SeqLoteModelo AND TIPOLOTE = 'M';
   --
   -- Se o dia da semana for igual hoje, retorna hoje
-   IF v_diasemana = v_diahoje
+   IF v_diasemana = v_dia 
+  AND v_dta = TRUNC(SYSDATE)
 
    THEN
      SELECT TRUNC(SYSDATE)
        INTO v_proxped
        FROM DUAL;
-       
-   DBMS_OUTPUT.PUT_LINE(v_diahoje||'-'||v_diasemana); -- Teste
+
+   DBMS_OUTPUT.PUT_LINE(v_dia||'-'||v_diasemana); -- Teste
   -- Caso contrário, próximo dia da semana que está parametrizado
    ELSE
 
-  SELECT NEXT_DAY((SELECT MAX(DTAHORINCLUSAO)
+  SELECT NEXT_DAY((SELECT MAX(TRUNC(DTAHORINCLUSAO))
     FROM CONSINCO.MAC_GERCOMPRA A 
    WHERE 1=1
-     AND A.SEQGERMODELOCOMPRA = p_SeqLoteModelo)
+     AND A.SEQGERMODELOCOMPRA = p_SeqLoteModelo 
+      OR A.SEQGERCOMPRA = p_SeqLoteModelo AND TIPOLOTE = 'M')
    + v_diasconfig, v_diasemana)
    INTO v_proxped
    FROM DUAL;
 
-   END IF;
+    END IF;
 
  RETURN v_proxped;
  EXCEPTION
