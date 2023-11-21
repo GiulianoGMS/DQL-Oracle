86d89859e3b88c1f71badab971f27325745b1241
Giuliano Gomes <91477669+Giulianogms@users.noreply.github.com>
2023-11-21 15:44:13 -0300

    Update FIs_Nfs Nat Desp.sql

--- FIs_Nfs Nat Desp.sql
+++ FIs_Nfs Nat Desp.sql
@@ -1,37 +1,23 @@
-SELECT A.NROEMPRESA EMPRESA, A.NRONOTA, 
+SELECT A.NROEMPRESA EMPRESA, A.NRONOTA, C.NROPARCELA, 
        SUBSTR(B.NOMERAZAO, 0,35) NOMERAZAO, 
        LPAD(B.NROCGCCPF,12,0)||LPAD(B.DIGCGCCPF,2,0) CNPJ,
        TO_CHAR(A.DTAEMISSAO,'DD/MM/YYYY') EMISSAO, 
        TO_CHAR(A.DTAENTRADA,'DD/MM/YYYY') ENTRADA,  
        TO_CHAR(C.DTAVENCTO, 'DD/MM/YYYY') VENCIMENTO, 
-       TO_CHAR(C.VALOR, 'FM999G999G990D90', 'NLS_NUMERIC_CHARACTERS='',.''') VALOR
+       TO_CHAR(C.VALOR, 'FM999G999G990D90', 'NLS_NUMERIC_CHARACTERS='',.''') VALOR,
+       TO_CHAR((SELECT SUM(C.VALOR) 
+        FROM CONSINCO.OR_NFDESPESA A INNER JOIN CONSINCO.GE_PESSOA       B ON A.SEQPESSOA = B.SEQPESSOA
+                                     INNER JOIN CONSINCO.OR_NFVENCIMENTO C ON A.SEQNOTA = C.SEQNOTA
+                             
+       WHERE A.DTAENTRADA BETWEEN :DT1 AND :DT2
+       AND A.CODHISTORICO = :NR1
+       AND A.NROEMPRESA IN (#LS1)), 'FM999G999G990D90', 'NLS_NUMERIC_CHARACTERS='',.''') VALOR_TOTAL,
+        TO_CHAR(:DT1, 'DD/MM/YYYY') DT1,TO_CHAR(:DT2, 'DD/MM/YYYY') DT2, A.CODHISTORICO COD_NAT
 
 FROM CONSINCO.OR_NFDESPESA A INNER JOIN CONSINCO.GE_PESSOA       B ON A.SEQPESSOA = B.SEQPESSOA
                              INNER JOIN CONSINCO.OR_NFVENCIMENTO C ON A.SEQNOTA = C.SEQNOTA
                              
-WHERE A.DTAENTRADA BETWEEN SYSDATE - 100 AND SYSDATE
-  AND A.CODHISTORICO = 746
-  AND A.NROEMPRESA   IN (1,2,3,4,6,7,8,9,10)
-
-/*
-union all
-
-select null, null, null, null,
- null, null,  
-         null, sum(c.valor),
-          null, null ,null ,null ,null,null, null ,null ,null ,null,null, null ,null ,null ,null,null, null ,null ,null ,null,
-          null,null, null ,null ,null ,null,null, null ,null ,null ,null,null, null ,null ,null ,null,
-          null,
-          null,
-          null,
-          null,
-          null,
-         null,
-          null,
-         null
-from consinco.or_nfdespesa a inner join consinco.ge_pessoa b on (a.seqpessoa = b.seqpessoa)
-                    inner join consinco.or_nfvencimento c on (a.seqnota = c.seqnota)
-where a.dtaentrada between :DT1 and :DT2
-and a.codhistorico = :NR1
-and a.nroempresa in (#LS1)
-order by 5, 2,1*/
+WHERE A.DTAENTRADA BETWEEN :DT1 AND :DT2
+  AND TO_CHAR(A.CODHISTORICO) IN (#LT1)
+  AND A.NROEMPRESA IN (#LS1)
+ORDER BY 1,2,3
