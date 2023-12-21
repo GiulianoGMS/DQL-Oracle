d2f4049787334e57019c3612ad373de4ef159096
Giuliano Gomes <91477669+Giulianogms@users.noreply.github.com>
2023-12-21 13:07:21 -0300

    Create Com_Incons Vda Tab Emp 503.sql

---
+++ Com_Incons Vda Tab Emp 503.sql
@@ -0,0 +1,14 @@
+-- Criar crítica em MAD_CRITICAPEDCONFIG 
+
+-- Inserir select em MADV_CRITICAPEDVENDA
+
+-- Trava utilização errada de tabela de venda na Emp 503 - Importação
+
+SELECT DISTINCT A.NROPEDVENDA, A.NROEMPRESA, 'Tabela de venda incorreta' CODCRITICA
+   FROM CONSINCO.MAD_PEDVENDA A
+   
+   WHERE 1=1
+   AND A.NROEMPRESA   = 503
+   AND A.NROTABVENDA  = 99
+   AND A.CODGERALOPER = 64
+   AND A.SITUACAOPED != 'C'
