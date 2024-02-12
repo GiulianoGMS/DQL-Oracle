c5d59a89d54b62d8992edf34717637708fe73e67
Giuliano Gomes <91477669+GiulianoGMS@users.noreply.github.com>
2024-02-12 10:51:54 -0300

    Update Fis_Incons Imp Ret.sql

--- Fis_Incons Imp Ret.sql
+++ Fis_Incons Imp Ret.sql
@@ -3,7 +3,7 @@ ALTER SESSION SET CURRENT_SCHEMA = CONSINCO;
 -- Adicionar em MLFV_AUXNOTAFISCALINCONS
 
 -- Criado por Giuliano em 26/01/2024 - Solic Neides Ticket 341549
--- Barra Imp Ret Nulo com CST 60
+-- Barra Imp Ret Nulo ou Zerado com CST 60
 
 SELECT DISTINCT (A.SEQAUXNOTAFISCAL) AS SEQAUXNOTAFISCAL,
                 A.NUMERONF,
@@ -12,11 +12,11 @@ SELECT DISTINCT (A.SEQAUXNOTAFISCAL) AS SEQAUXNOTAFISCAL,
                 'B' AS BLOQAUTOR,
                 77  AS CODINCONSISTENC,
                 'O(s) Campo(s): '||
-                CASE WHEN M014_VL_OP_PROP_DIST IS NULL THEN 'vICMSSubstituto' ELSE NULL END||
-                CASE WHEN M014_VL_BC_ST_RET    IS NULL THEN ' vBCSTRet'       ELSE NULL END||
-                CASE WHEN M014_VL_ICMS_ST_RET  IS NULL THEN ' vICMSSTRet'     ELSE NULL END||
-              --CASE WHEN M014_VL_BC_FCP_RET   IS NULL THEN ' vBCFCPSTRet'    ELSE NULL END||
-              --CASE WHEN M014_VL_FCP_RET      IS NULL THEN ' vFCPSTRet'      ELSE NULL END||
+                CASE WHEN NVL(L.M014_VL_OP_PROP_DIST,0) = 0 THEN 'vICMSSubstituto' ELSE NULL END||
+                CASE WHEN NVL(L.M014_VL_BC_ST_RET,0)    = 0 THEN ' vBCSTRet'       ELSE NULL END||
+                CASE WHEN NVL(L.M014_VL_ICMS_ST_RET,0)  = 0 THEN ' vICMSSTRet'     ELSE NULL END||
+                --CASE WHEN M014_VL_BC_FCP_RET   IS NULL THEN ' vBCFCPSTRet'    ELSE NULL END||
+                --CASE WHEN M014_VL_FCP_RET      IS NULL THEN ' vFCPSTRet'      ELSE NULL END||
                 ' esta(o) nulo(s) no XML! Entre em contato com o Departamento Fiscal'
                 MENSAGEM
 
@@ -28,13 +28,12 @@ SELECT DISTINCT (A.SEQAUXNOTAFISCAL) AS SEQAUXNOTAFISCAL,
                                     INNER JOIN TMP_M014_ITEM L ON (L.M000_ID_NF = K.M000_ID_NF AND L.M014_NR_ITEM = B.SEQITEMNFXML)
 
 WHERE A.CODGERALOPER = 1
-  AND A.NROEMPRESA IN (501,11,8,26)
+  AND A.NROEMPRESA IN (501,11,8,26) -- Solicitadas por Neides
   AND A.SEQPESSOA NOT IN (SELECT SEQPESSOA FROM GE_PESSOA G WHERE G.NROCGCCPF = 236433150110) -- Criar De/Para Posteriormente
   AND L.M014_DM_TRIB_ICMS = 8 -- De/Para na Function fc5_RetIndSituacaoNF_NFe - Regra barra apenas CST 60
   -- Critérios que nao podem estar nulos
-  AND (L.M014_VL_OP_PROP_DIST IS NULL  -- vICMSSubstituto
-   OR  L.M014_VL_BC_ST_RET    IS NULL  -- vBCSTRet
-   OR  L.M014_VL_ICMS_ST_RET  IS NULL)  -- vICMSSTRet
- --OR  L.M014_VL_BC_FCP_RET   IS NULL  -- vBCFCPSTRet
--- OR  L.M014_VL_FCP_RET      IS NULL) -- vFCPSTRet
-   
+  AND (NVL(L.M014_VL_OP_PROP_DIST,0) = 0  -- vICMSSubstituto
+   OR  NVL(L.M014_VL_BC_ST_RET,0)    = 0  -- vBCSTRet
+   OR  NVL(L.M014_VL_ICMS_ST_RET,0)  = 0)  -- vICMSSTRet
+ --OR  L.M014_VL_BC_FCP_RET   IS NULL  -- vBCFCPSTRet -- Removidos - Solic Neides
+ --OR  L.M014_VL_FCP_RET      IS NULL) -- vFCPSTRet   -- ^
