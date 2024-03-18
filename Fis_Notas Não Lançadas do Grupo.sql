b6d13d01bd953898494238de01bf13e32e253a84
Giuliano Gomes <91477669+GiulianoGMS@users.noreply.github.com>
2024-03-18 08:56:08 -0300

    Update Fis_Notas Não Lançadas do Grupo.sql

--- Fis_Notas Não Lançadas do Grupo.sql
+++ Fis_Notas Não Lançadas do Grupo.sql
@@ -3,7 +3,7 @@ SELECT A.SEQPESSOA EMP_DESTINO, CASE WHEN A.NUMERONF IN(SELECT Z.NRONOTA FROM CO
          THEN 'NF Lançada no Orçamento' ELSE 'NF Não Importada' END STATUS, A.NROEMPRESA EMITENTE, A.NUMERONF, 
        TO_CHAR(A.DTAEMISSAO, 'DD/MM/YYYY') DTAEMISSAO, 
        CASE WHEN X.NUMERONF IS NULL THEN NULL ELSE X.NUMERONF||' - CGO: '||X.CODGERALOPER||' - Data Emissão: '
-                                                             ||TO_CHAR(X.DTAEMISSAO, 'DD/MM/YYYY') END NF_MATOU_OP
+                                                             ||TO_CHAR(X.DTAEMISSAO, 'DD/MM/YYYY') END NF_MATOU_OP, A.OBSERVACAO INFO_AD
 
   FROM MLF_NOTAFISCAL A LEFT JOIN MAX_CODGERALOPER C ON A.CODGERALOPER = C.CODGERALOPER
                         LEFT JOIN MLF_NOTAFISCAL X   ON A.NUMERONF     = X.NFREFERENCIANRO
@@ -33,7 +33,7 @@ SELECT D.NROEMPRESA DESTINO,
        DECODE(D.INDPROCESSAMENTO, 'G', 'NF em Conferência', 'L', 'NF Liberada, Conferência Não Iniciada', 'NF Gerada, Processo Não Iniciado') END,
        D.SEQPESSOA EMITENTE, D.NUMERONF, TO_CHAR(D.DTAEMISSAO, 'DD/MM/YYYY'), 
        CASE WHEN X.NUMERONF IS NULL THEN NULL ELSE X.NUMERONF||' - CGO: '||X.CODGERALOPER||' - Data Emissão: '
-                                                             ||TO_CHAR(X.DTAEMISSAO, 'DD/MM/YYYY') END NF_MATOU_OP
+                                                             ||TO_CHAR(X.DTAEMISSAO, 'DD/MM/YYYY') END NF_MATOU_OP, D.OBSERVACAO
   FROM MLF_AUXNOTAFISCAL D LEFT JOIN MAX_CODGERALOPER C ON D.CODGERALOPER = C.CODGERALOPER
                            LEFT JOIN MLF_NOTAFISCAL X   ON D.NUMERONF     = X.NFREFERENCIANRO
                                                     AND X.CODGERALOPER IN (202,16,69,135,22,141,12,169,135,27,23,26,953,923)
@@ -43,9 +43,9 @@ SELECT D.NROEMPRESA DESTINO,
                                                     AND X.STATUSNF != 'C'
                                                     
  WHERE D.SEQPESSOA < 999 
-   AND D.SEQPESSOA != D.NROEMPRESA
    AND D.NUMERONF != 0   
    AND D.DTAEMISSAO  BETWEEN :DT1 AND :DT2
+AND D.SEQPESSOA != D.NROEMPRESA
    AND D.NROEMPRESA in (#LS1)
    AND D.STATUSNF != 'C'
    AND D.NUMERONF NOT IN (SELECT NUMERONF FROM MLF_NOTAFISCAL B WHERE B.TIPNOTAFISCAL = 'E' 
