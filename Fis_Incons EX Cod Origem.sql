b6bd0b128f72effededf35d5a56ce2901991d8bf
Giuliano Gomes <91477669+GiulianoGMS@users.noreply.github.com>
2024-07-30 08:48:23 -0300

    Update and rename Fis_Incons Cod Origem EX.sql to Fis_Incons EX Cod Origem.sql

--- Fis_Incons EX Cod Origem.sql
+++ Fis_Incons EX Cod Origem.sql
@@ -8,13 +8,12 @@ SELECT DISTINCT (A.SEQAUXNOTAFISCAL) AS SEQAUXNOTAFISCAL,
                 0   AS SEQAUXNFITEM,
                 'B' AS BLOQAUTOR,
                 76  AS CODINCONSISTENC,
-                'Cod. Origem da Mercadoria Oriunda EX deve ser 1. PLU: '||B.SEQPRODUTO MENSAGEM
+                '(EX) Cod. Origem da Mercadoria Oriunda EX deve ser 1. PLU: '||B.SEQPRODUTO||' - Entrar em contato com Depto Cadastro Comercial' MENSAGEM
 
   FROM CONSINCO.MLF_AUXNOTAFISCAL A INNER JOIN CONSINCO.MLF_AUXNFITEM B ON A.SEQAUXNOTAFISCAL = B.SEQAUXNOTAFISCAL
                                     INNER JOIN CONSINCO.GE_PESSOA G ON G.SEQPESSOA = A.SEQPESSOA
                                     INNER JOIN CONSINCO.MAP_PRODUTO P ON P.SEQPRODUTO = B.SEQPRODUTO
                                     INNER JOIN CONSINCO.MAP_FAMDIVISAO F ON F.SEQFAMILIA = P.SEQFAMILIA
-    
 WHERE G.UF = 'EX'
   AND A.CODGERALOPER IN (43,5)
   AND F.CODORIGEMTRIB != 1 -- Olhar a tributacao da familia pois é o considerado na emissao
