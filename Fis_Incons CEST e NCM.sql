b76a8eaa1240e59d4622a453b90e58366c9fd20a
Giuliano Gomes <91477669+GiulianoGMS@users.noreply.github.com>
2024-09-10 12:46:40 -0300

    Update Fis_Incons CEST e NCM.sql

--- Fis_Incons CEST e NCM.sql
+++ Fis_Incons CEST e NCM.sql
@@ -11,11 +11,11 @@ SELECT DISTINCT (A.SEQAUXNOTAFISCAL) AS SEQAUXNOTAFISCAL,
                 'L' AS BLOQAUTOR,
                 73  AS CODINCONSISTENC,
                 CASE WHEN L.CODCEST IS NULL                                                                    THEN 'Cod. CEST do produto '||
-                  B.SEQPRODUTO||' no XML está NULO - Abrir chamado para do Departamento Fiscal - CEST XML: '||NVL(TO_CHAR(L.CODCEST), 'NULO')||' - C5: '||E.CODCEST
-                    -- WHEN NVL(L.CODCEST,0) != NVL(E.CODCEST,0) AND NVL(L.M014_CD_NCM,0) != NVL(CODNBMSH,0)   THEN 'Codigo CEST e NCM do produto '||B.SEQPRODUTO||' no  XML estão divergentes do cadastro no sistema - Abrir chamado para o Depto Cadastro Tributário'
+                  B.SEQPRODUTO||' no XML esta NULO - Abrir chamado para do Departamento Fiscal - CEST XML: '||NVL(TO_CHAR(L.CODCEST), 'NULO')||' - C5: '||E.CODCEST
+                    -- WHEN NVL(L.CODCEST,0) != NVL(E.CODCEST,0) AND NVL(L.M014_CD_NCM,0) != NVL(CODNBMSH,0)   THEN 'Codigo CEST e NCM do produto '||B.SEQPRODUTO||' no  XML estao divergentes do cadastro no sistema - Abrir chamado para o Depto Cadastro Tributario'
                      WHEN NVL(L.CODCEST,0) != NVL(E.CODCEST,0) /*AND NVL(L.M014_CD_NCM,0)  = NVL(CODNBMSH,0) */THEN 'Cod. CEST do produto '||
-                  B.SEQPRODUTO||' está divergente do cadastro no sistema - Abrir chamado para o Depto Cadastro Tributário - CEST XML: '||L.CODCEST||' - C5: '||E.CODCEST
-                    -- WHEN NVL(L.CODCEST,0)  = NVL(E.CODCEST,0) AND NVL(L.M014_CD_NCM,0) != NVL(CODNBMSH,0)   THEN 'Codigo NCM do produto '||B.SEQPRODUTO||' está divergente do cadastro no sistema - Abrir chamado para o Depto Cadastro Tributário'
+                  B.SEQPRODUTO||' esta divergente do cadastro no sistema - Abrir chamado para o Depto Cadastro Tributario - CEST XML: '||L.CODCEST||' - C5: '||E.CODCEST
+                    -- WHEN NVL(L.CODCEST,0)  = NVL(E.CODCEST,0) AND NVL(L.M014_CD_NCM,0) != NVL(CODNBMSH,0)   THEN 'Codigo NCM do produto '||B.SEQPRODUTO||' esta divergente do cadastro no sistema - Abrir chamado para o Depto Cadastro Tributario'
                        END AS MENSAGEM
 
   FROM CONSINCO.MLF_AUXNOTAFISCAL A INNER JOIN CONSINCO.MLF_AUXNFITEM B ON A.SEQAUXNOTAFISCAL = B.SEQAUXNOTAFISCAL
@@ -26,11 +26,12 @@ SELECT DISTINCT (A.SEQAUXNOTAFISCAL) AS SEQAUXNOTAFISCAL,
                                     INNER JOIN TMP_M014_ITEM L ON (L.M000_ID_NF = K.M000_ID_NF AND L.M014_NR_ITEM = B.SEQITEMNFXML)
 
 WHERE A.CODGERALOPER = 1
-  AND L.M014_DM_TRIB_ICMS IN (1,9,8) -- De/Para na Function fc5_RetIndSituacaoNF_NFe - Regra Barra CST 10 70 60 respectivamente na clausula
+    -- De/Para na Function fc5_RetIndSituacaoNF_NFe - Regra Barra CST 10 70 60 respectivamente na clausula
+  AND (L.M014_DM_TRIB_ICMS IN (1,9,8) OR NVL(B.VLRICMSST,0) > 0) -- VLRICMSST > 0 - Giuliano 10/09/24 - Solic Danielle
                                      -- CST 202 e 500 (Simples Nacional) sera tratado no OR abaixo pois muda a regra
   AND (NVL(L.CODCEST,0) != NVL(E.CODCEST,1))-- OR NVL(L.M014_CD_NCM,0) != NVL(CODNBMSH,0))
   -- Trata SN
   OR A.CODGERALOPER = 1
  AND EXISTS(SELECT 1 FROM MAF_FORNECEDOR SN WHERE SN.MICROEMPRESA = 'S' AND SEQFORNECEDOR = A.SEQPESSOA)
- AND M014_CD_CFOP NOT IN (5401,5101,5102,6102)
+ AND M014_CD_CFOP NOT IN (5401,5101,5102,6102,6401)
  AND (NVL(L.CODCEST,0) != NVL(E.CODCEST,1))-- OR NVL(L.M014_CD_NCM,0) != NVL(CODNBMSH,0))*/
