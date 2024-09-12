0f2a4f3b268a10fd161d80319d6227f37a24b09b
Giuliano Gomes <91477669+GiulianoGMS@users.noreply.github.com>
2024-09-12 16:31:38 -0300

    Update Imp_Ext Lote Infos.sql

--- Imp_Ext Lote Infos.sql
+++ Imp_Ext Lote Infos.sql
@@ -1,3 +1,5 @@
+-- XLS em https://github.com/GiulianoGMS/Excel-Files/blob/main/Rel_Aux_ImportacaoDireta.xlsx
+
 ALTER SESSION SET CURRENT_SCHEMA = CONSINCO;
 
 SELECT A.SEQPEDIDOIMPORT NRO_PED,
@@ -23,13 +25,23 @@ SELECT A.SEQPEDIDOIMPORT NRO_PED,
        A.VLRDESPESA,
        A.VLRDESPESAFORA,
        A.VLRFCPICMS,
-       A.VLRFCPST
+       A.VLRFCPST,
+       T.PERPISDIF,
+       T.PERCOFINSDIF,
+       T.PERALIQUOTA ALIQICMS
+       
   FROM MAD_PIPEDIMPORTPROD A INNER JOIN MAP_PRODUTO B      ON A.SEQPRODUTO = B.SEQPRODUTO
                               LEFT JOIN MAP_PRODCODIGO C   ON C.SEQPRODUTO = A.SEQPRODUTO 
                                                           AND C.TIPCODIGO = 'F'
                              INNER JOIN MAP_FAMEMBALAGEM D ON D.SEQFAMILIA = B.SEQFAMILIA
                                                           AND D.QTDEMBALAGEM = A.QTDEMBALAGEM
+                             INNER JOIN MAP_FAMDIVISAO F   ON F.SEQFAMILIA = B.SEQFAMILIA
+                             INNER JOIN MAP_TRIBUTACAOUF T ON T.NROTRIBUTACAO = F.NROTRIBUTACAO AND T.NROREGTRIBUTACAO = 8 /*IMP DIRETA*/ 
+                                                                                                AND T.UFEMPRESA = 'SP' 
+                                                                                                AND T.UFCLIENTEFORNEC = 'EX' 
+                                                                                                AND T.TIPTRIBUTACAO = 'EI'
  WHERE 1 = 1
-   AND A.SEQPEDIDOIMPORT = '1598'
+   AND A.SEQPEDIDOIMPORT = '1664'
    
 ORDER BY 10 DESC
+
