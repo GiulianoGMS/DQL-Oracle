d8dc311910a80c05569ef798165b5cb387477a11
Giuliano Gomes <91477669+GiulianoGMS@users.noreply.github.com>
2024-10-01 15:13:58 -0300

    Update Fis_Notas  Manuais.sql

--- Fis_Notas  Manuais.sql
+++ Fis_Notas  Manuais.sql
@@ -1,29 +1,46 @@
-select a.nroempresa EMPRESA,
-       a.nronota NOTA,
-       a.serie SERIE,
-       (select g.descricao  from CONSINCO.aba_historico g where g.seqhistorico = a.codhistorico) Historico,
-       to_char(a.dtaemissao,'DD/MM/YYYY') EMISSAO,
-        to_char(a.dtaentrada,'DD/MM/YYYY') ENTRADA,
-        a.cgo CGO,
-        a.cfop,
-        (select y.nomerazao from CONSINCO.ge_pessoa y where y.seqpessoa = a.seqpessoa) RAZAO,
-         to_char(a.valor,'FM999G999G999D90', 'nls_numeric_characters='',.''') VALOR,
-         (SELECT COUNT(DISTINCT(A.SEQNOTA))
-          from CONSINCO.or_nfdespesa a
-          where a.codmodelo in (01,06,21,22,28,29)
-          and a.dtaentrada = DATE '2023-02-02'
-          and a.nroempresa in (02)) QTD_TOTAL,
-         (SELECT to_char(sum(a.valor),'FM999G999G999D90', 'nls_numeric_characters='',.''')
-          from CONSINCO.or_nfdespesa a
-          where a.codmodelo in (01,06,21,22,28,29)
-          and a.dtaentrada = DATE '2023-02-02'
-          and a.nroempresa in (02)) VALOR_TOTAL,
-          TO_CHAR(:DT1, 'DD/MM/YYYY') Data_Inicial, TO_CHAR(:DT2, 'DD/MM/YYYY') Data_Final
-          
-from CONSINCO.or_nfdespesa a
-where a.codmodelo in (01,06,21,22,28,29)
-and a.dtaentrada BETWEEN :DT1 AND :DT2
-and a.nroempresa in (#LS1)
+SELECT DISTINCT AA.*, 
+       CASE WHEN '#LS1' IS NULL THEN 'TODAS AS EMPRESAS' ELSE TO_CHAR(B.NROEMPRESA) END EMP2, 
+       TO_CHAR(:DT1, 'DD/MM/YYYY') DT1, TO_CHAR(:DT2, 'DD/MM/YYYY') DT2
 
+  FROM GE_EMPRESA B LEFT JOIN (SELECT A.NROEMPRESA EMPRESA,
+       A.NRONOTA NOTA,
+       A.SERIE SERIE,
+     
+  (SELECT G.DESCRICAO
+          FROM CONSINCO.ABA_HISTORICO G
+         WHERE G.SEQHISTORICO = A.CODHISTORICO) HISTORICO,
+       TO_CHAR(A.DTAEMISSAO, 'DD/MM/YYYY') EMISSAO,
+       TO_CHAR(A.DTAENTRADA, 'DD/MM/YYYY') ENTRADA,
+       A.CGO CGO,
+       A.CFOP,
+   
+    (SELECT Y.NOMERAZAO
+          FROM CONSINCO.GE_PESSOA Y
+         WHERE Y.SEQPESSOA = A.SEQPESSOA) RAZAO,
+       TO_CHAR(A.VALOR, 'FM999G999G999D90', 'nls_numeric_characters='',.''') VALOR,
+       (SELECT COUNT(DISTINCT(A.SEQNOTA))
+         
+ FROM CONSINCO.OR_NFDESPESA A
+         WHERE A.CODMODELO IN (01, 06, 21, 22, 28, 29)
+               AND A.DTAENTRADA BETWEEN :DT1 AND :DT2
+               AND A.NROEMPRESA IN (#LS1)) QTD_TOTAL,
+    
+   (SELECT TO_CHAR(SUM(A.VALOR),
+                       'FM999G999G999D90',
+                       'nls_numeric_characters='',.''')
+          FROM CONSINCO.OR_NFDESPESA A
+         WHERE A.CODMODELO IN (01, 06, 21, 22, 28, 29)
+               AND A.DTAENTRADA BETWEEN :DT1 AND :DT2
+               AND A.NROEMPRESA IN (#LS1)) VALOR_TOTAL,
+       TO_CHAR(:DT1, 'DD/MM/YYYY') DATA_INICIAL,
+       TO_CHAR(:DT2, 'DD/MM/YYYY') DATA_FINAL
+        FROM CONSINCO.OR_NFDESPESA A
+       
+        WHERE A.CODMODELO IN (01, 06, 21, 22, 28, 29)
+       AND A.DTAENTRADA BETWEEN :DT1 AND :DT2
+       AND A.NROEMPRESA IN (#LS1)
+       
+       ) AA ON AA.EMPRESA = B.NROEMPRESA
 
-
+       WHERE B.NROEMPRESA IN (#LS1)
+ ORDER BY 1
