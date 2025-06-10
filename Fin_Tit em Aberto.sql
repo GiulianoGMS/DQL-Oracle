7f526a77efd8c8f519402a4d55c36ab3c4b0588b
Giuliano Gomes <91477669+GiulianoGMS@users.noreply.github.com>
2025-06-10 11:18:26 -0300

    Create Fin_Tit em Aberto.sql

---
+++ Fin_Tit em Aberto.sql
@@ -0,0 +1,71 @@
+SELECT F.NROEMPRESA, F.NROTITULO, F.SEQPESSOA||' - '||G.NOMERAZAO FORNEC,
+       F.DTAVENCIMENTO, 
+       TO_CHAR(F.VLRORIGINAL, 'FM999G999G990D90', 'NLS_NUMERIC_CHARACTERS='',.''') VLR_TITULO,
+       TO_CHAR(NVL(C.VLRDESCCONTRATO,0), 'FM999G999G990D90', 'NLS_NUMERIC_CHARACTERS='',.''') VLR_DESC,
+       F.OBSERVACAO
+
+  FROM FI_TITULO F LEFT JOIN FI_COMPLTITULO C ON F.SEQTITULO = C.SEQTITULO
+                   INNER JOIN GE_PESSOA G      ON G.SEQPESSOA = F.SEQPESSOA
+                   INNER JOIN FI_FORNECEDOR  O ON O.SEQPESSOA = G.SEQPESSOA
+                   
+ WHERE F.ABERTOQUITADO = 'A'
+   AND F.OBRIGDIREITO = 'O'
+   AND F.DTAEMISSAO BETWEEN SYSDATE - 1 AND SYSDATE
+   AND F.CODESPECIE IN ('AGUA',
+                        'ALUGPG',
+                        'ANTREC',
+                        'ATIVO',
+                        'ATIVOC',
+                        'ATVEFU',
+                        'BEFUNC',
+                        'COFINS',
+                        'COSIND',
+                        'CSSLL',
+                        'DESP',
+                        'DIVSOC',
+                        'DUPP',
+                        'DVRBEC',
+                        'ENERGI',
+                        'ESPORT',
+                        'FATDMC',
+                        'FATCID',
+                        'FGTS',
+                        'FGTSQT',
+                        'FRETE',
+                        'GNRE',
+                        'ICMS',
+                        'IMPOST',
+                        'INSS',
+                        'INSSNF',
+                        'IPI',
+                        'IPTU',
+                        'IR',
+                        'IRRFFP',
+                        'IRRNF',
+                        'ISSQN',
+                        'ISSQNP',
+                        'ISST',
+                        'LEBEMC',
+                        'LEASIM',
+                        'LEASIN',
+                        'LEIROU',
+                        'MKFUNC',
+                        'MKPREM',
+                        'PAGEST',
+                        'PCCNF',
+                        'PIS',
+                        'PROCIM',
+                        'PROCIV',
+                        'PROTRG',
+                        'RECARG',
+                        'REEMB',
+                        'RETSOC',
+                        'SEGURO',
+                        'SERVPJ',
+                        'SEST',
+                        'SUPERT',
+                        'TEL',
+                        'VLDESC')
+                                    
+  ORDER BY 3,1
+ 
