d3a53a79d3055108a0f3fe1c42f2bcf668932e04
Giuliano Gomes <91477669+GiulianoGMS@users.noreply.github.com>
2025-08-18 14:07:36 -0300

    Update Fis_Incons Dev NF do Grupo 802.sql

--- Fis_Incons Dev NF do Grupo 802.sql
+++ Fis_Incons Dev NF do Grupo 802.sql
@@ -4,15 +4,15 @@
 UNION ALL
 -- Ticket 619423
 -- Adicionado por Giuliano em 18/08/2025
--- CGO 802 apenas entre empresas do grupo                
+-- CGO 802 nao pode ser utilizado para fornec do grupo Nagumo            
 SELECT A.IDSESSION,
        A.INST_ID,
        A.SEQNFDEVFORNEC,
        A.SEQPRODUTO,
        3 CODCRITICA,
-       'Este CGO somente pode ser utilizado para fornecedores do grupo [NAGUMO]' MENSAGEM,
+       'Este CGO não pode ser utilizado para fornecedores do grupo [NAGUMO]' MENSAGEM,
        'B' INDBLOQUEIOLIBERA
   FROM MFLX_NFDEVFORNEC A INNER JOIN MAX_EMPRESA E ON E.NROEMPRESA = A.NROEMPRESA
  WHERE 1=1
    AND A.CODGERALOPER = 802
-   AND SEQFORNECEDOR > 999;
+   AND SEQFORNECEDOR < 999
