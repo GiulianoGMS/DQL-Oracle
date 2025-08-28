d5fc87ac8eddade685818d296bb744c528faa01b
Giuliano Gomes <91477669+GiulianoGMS@users.noreply.github.com>
2025-08-28 14:36:07 -0300

    Create Com_Libera Saldo Proc importacao.sql

---
+++ Com_Libera Saldo Proc importacao.sql
@@ -0,0 +1,6 @@
+-- Por N motivo, comprador lancou o saldo incorreto no processo
+-- É preciso liberar saldo alterando as linhas de CREDITO (INDCREDITODEBITO = 'C') na tabela:
+
+MAD_PILANCTOPAGTODESP
+
+-- Buscar pelo NroProcImporcatao (Numero do Processo de Importacao amarrado na DI e na NF);
